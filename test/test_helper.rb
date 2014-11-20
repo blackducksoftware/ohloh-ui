412365adf77787ab8304f44ae2442b2d3c1d99a3
bobofraggins <peter.fry@mac.com>
2014-11-20 10:48:45 -0500

    Jenkins plugin requires Rcov formatting.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 SimpleCov.start 'rails'
 
 class ActiveSupport::TestCase
