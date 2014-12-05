e5e728d6f1e228df72363b968800218b37ba5ae3
bobofraggins <peter.fry@mac.com>
2014-12-05 06:05:30 -0500

    OTWO-3277 Forgot to fix the rest of the call sites.

--- test/controllers/api_keys_controller_test.rb
+++ test/controllers/api_keys_controller_test.rb
@@ -11,13 +11,13 @@ class ApiKeysControllerTest < ActionController::TestCase
   end
 
   test 'unlogged in users should not be able to look at the global list of api keys' do
-    @controller.expects(:current_user).at_least_once.returns(nil)
+    login_as nil
     get :index
     assert_response :unauthorized
   end
 
   test 'normal users should not be able to look at the global list of api keys' do
-    @controller.expects(:current_user).at_least_once.returns(accounts(:user))
+    login_as accounts(:user)
     get :index
     assert_response :unauthorized
   end
@@ -25,7 +25,7 @@ class ApiKeysControllerTest < ActionController::TestCase
   test 'index page should find models whose name match the "q" parameter' do
     api_key1 = create(:api_key, name: 'foobar')
     api_key2 = create(:api_key, name: 'goobaz')
-    @controller.expects(:current_user).at_least_once.returns(accounts(:admin))
+    login_as accounts(:admin)
     get :index, q: 'foobar'
     assert_response :ok
     assert response.body.match(api_key1.name)
@@ -35,7 +35,7 @@ class ApiKeysControllerTest < ActionController::TestCase
   test 'index page should find models whose description match the "q" parameter' do
     api_key1 = create(:api_key, description: 'foobar')
     api_key2 = create(:api_key, description: 'goobaz')
-    @controller.expects(:current_user).at_least_once.returns(accounts(:admin))
+    login_as accounts(:admin)
     get :index, q: 'foobar'
     assert_response :ok
     assert response.body.match(api_key1.name)
@@ -45,7 +45,7 @@ class ApiKeysControllerTest < ActionController::TestCase
   test 'index page should find models whose key match the "q" parameter' do
     api_key1 = create(:api_key, key: 'foobar')
     api_key2 = create(:api_key, key: 'goobaz')
-    @controller.expects(:current_user).at_least_once.returns(accounts(:admin))
+    login_as accounts(:admin)
     get :index, q: 'foobar'
     assert_response :ok
     assert response.body.match(api_key1.name)
@@ -55,7 +55,7 @@ class ApiKeysControllerTest < ActionController::TestCase
   test 'index page should find models whose account name match the "q" parameter' do
     api_key1 = create(:api_key, account_id: accounts(:user).id)
     api_key2 = create(:api_key, account_id: accounts(:admin).id)
-    @controller.expects(:current_user).at_least_once.returns(accounts(:admin))
+    login_as accounts(:admin)
     get :index, q: 'user'
     assert_response :ok
     assert response.body.match(api_key1.name)
@@ -65,7 +65,7 @@ class ApiKeysControllerTest < ActionController::TestCase
   test 'index page should accept "query" in place of "q" as a parameter' do
     api_key1 = create(:api_key, name: 'foobar')
     api_key2 = create(:api_key, name: 'goobaz')
-    @controller.expects(:current_user).at_least_once.returns(accounts(:admin))
+    login_as accounts(:admin)
     get :index, query: 'foobar'
     assert_response :ok
     assert response.body.match(api_key1.name)
@@ -76,7 +76,7 @@ class ApiKeysControllerTest < ActionController::TestCase
     api_key1 = create(:api_key, last_access_at: Time.now)
     api_key2 = create(:api_key, last_access_at: Time.now - 1.day)
     api_key3 = create(:api_key, last_access_at: Time.now + 1.day)
-    @controller.expects(:current_user).at_least_once.returns(accounts(:admin))
+    login_as accounts(:admin)
     get :index, sort: 'most_recent_request'
     assert_response :ok
     assert(/#{api_key3.name}.*#{api_key1.name}.*#{api_key2.name}/m.match(response.body))
@@ -86,7 +86,7 @@ class ApiKeysControllerTest < ActionController::TestCase
     api_key1 = create(:api_key, daily_count: 11)
     api_key2 = create(:api_key, daily_count: 10)
     api_key3 = create(:api_key, daily_count: 12)
-    @controller.expects(:current_user).at_least_once.returns(accounts(:admin))
+    login_as accounts(:admin)
     get :index, sort: 'most_requests_today'
     assert_response :ok
     assert(/#{api_key3.name}.*#{api_key1.name}.*#{api_key2.name}/m.match(response.body))
@@ -96,7 +96,7 @@ class ApiKeysControllerTest < ActionController::TestCase
     api_key1 = create(:api_key, total_count: 11)
     api_key2 = create(:api_key, total_count: 10)
     api_key3 = create(:api_key, total_count: 12)
-    @controller.expects(:current_user).at_least_once.returns(accounts(:admin))
+    login_as accounts(:admin)
     get :index, sort: 'most_requests'
     assert_response :ok
     assert(/#{api_key3.name}.*#{api_key1.name}.*#{api_key2.name}/m.match(response.body))
@@ -106,7 +106,7 @@ class ApiKeysControllerTest < ActionController::TestCase
     api_key1 = create(:api_key, created_at: Time.now)
     api_key2 = create(:api_key, created_at: Time.now - 1.day)
     api_key3 = create(:api_key, created_at: Time.now + 1.day)
-    @controller.expects(:current_user).at_least_once.returns(accounts(:admin))
+    login_as accounts(:admin)
     get :index, sort: 'newest'
     assert_response :ok
     assert(/#{api_key3.name}.*#{api_key1.name}.*#{api_key2.name}/m.match(response.body))
@@ -116,7 +116,7 @@ class ApiKeysControllerTest < ActionController::TestCase
     api_key1 = create(:api_key, created_at: Time.now)
     api_key2 = create(:api_key, created_at: Time.now - 1.day)
     api_key3 = create(:api_key, created_at: Time.now + 1.day)
-    @controller.expects(:current_user).at_least_once.returns(accounts(:admin))
+    login_as accounts(:admin)
     get :index, sort: 'oldest'
     assert_response :ok
     assert(/#{api_key2.name}.*#{api_key1.name}.*#{api_key3.name}/m.match(response.body))
@@ -125,7 +125,7 @@ class ApiKeysControllerTest < ActionController::TestCase
   test 'index page should honor the sort order "account_name"' do
     api_key1 = create(:api_key, account_id: accounts(:user).id)
     api_key2 = create(:api_key, account_id: accounts(:admin).id)
-    @controller.expects(:current_user).at_least_once.returns(accounts(:admin))
+    login_as accounts(:admin)
     get :index, sort: 'account_name'
     assert_response :ok
     assert(/#{api_key2.name}.*#{api_key1.name}/m.match(response.body))
@@ -135,32 +135,32 @@ class ApiKeysControllerTest < ActionController::TestCase
     api_key1 = create(:api_key, created_at: Time.now)
     api_key2 = create(:api_key, created_at: Time.now - 1.day)
     api_key3 = create(:api_key, created_at: Time.now + 1.day)
-    @controller.expects(:current_user).at_least_once.returns(accounts(:admin))
+    login_as accounts(:admin)
     get :index, sort: 'I_am_a_banana!'
     assert_response :ok
     assert(/#{api_key3.name}.*#{api_key1.name}.*#{api_key2.name}/m.match(response.body))
   end
 
   test 'admins should be able to look at the api keys of someone else' do
-    @controller.expects(:current_user).at_least_once.returns(accounts(:admin))
+    login_as accounts(:admin)
     get :index, account_id: accounts(:user).id
     assert_response :ok
   end
 
   test 'unlogged in users should not be able to look at the api keys of a user' do
-    @controller.expects(:current_user).at_least_once.returns(nil)
+    login_as nil
     get :index, account_id: accounts(:user).id
     assert_response :unauthorized
   end
 
   test 'normal users should be able to look at their own api keys' do
-    @controller.expects(:current_user).at_least_once.returns(accounts(:user))
+    login_as accounts(:user)
     get :index, account_id: accounts(:user).id
     assert_response :ok
   end
 
   test 'normal users should not be able to look at someone elses api keys' do
-    @controller.expects(:current_user).at_least_once.returns(accounts(:user))
+    login_as accounts(:user)
     get :index, account_id: accounts(:admin).id
     assert_response :unauthorized
   end
@@ -168,7 +168,7 @@ class ApiKeysControllerTest < ActionController::TestCase
   test 'normal users should see their api keys, but not others' do
     api_key1 = create(:api_key, account_id: accounts(:user).id)
     api_key2 = create(:api_key, account_id: accounts(:admin).id)
-    @controller.expects(:current_user).at_least_once.returns(accounts(:user))
+    login_as accounts(:user)
     get :index, account_id: accounts(:user).id
     assert_response :ok
     assert response.body.match(api_key1.name)
@@ -178,7 +178,7 @@ class ApiKeysControllerTest < ActionController::TestCase
   test 'admins should be able to download a csv of the global list of api keys' do
     api_key1 = create(:api_key, account_id: accounts(:user).id)
     api_key2 = create(:api_key, account_id: accounts(:admin).id)
-    @controller.expects(:current_user).at_least_once.returns(accounts(:admin))
+    login_as accounts(:admin)
     get :index, format: :csv
     assert_response :ok
     assert response.body.match('Open Hub account,Application Name')
@@ -191,14 +191,14 @@ class ApiKeysControllerTest < ActionController::TestCase
 
   # new action
   test 'new should let admins edit daily limit' do
-    @controller.expects(:current_user).at_least_once.returns(accounts(:admin))
+    login_as accounts(:admin)
     get :new, account_id: accounts(:admin).id
     assert_response :ok
     assert response.body.match('api_key_daily_limit')
   end
 
   test 'new should not let users edit daily limit' do
-    @controller.expects(:current_user).at_least_once.returns(accounts(:user))
+    login_as accounts(:user)
     get :new, account_id: accounts(:user).id
     assert_response :ok
     assert !response.body.match('api_key_daily_limit')
@@ -206,14 +206,14 @@ class ApiKeysControllerTest < ActionController::TestCase
 
   test 'new should not let users who have enough keys make more' do
     (1..ApiKey::KEY_LIMIT_PER_ACCOUNT).each { create(:api_key, account_id: accounts(:user).id) }
-    @controller.expects(:current_user).at_least_once.returns(accounts(:user))
+    login_as accounts(:user)
     get :new, account_id: accounts(:user).id
     assert_response 302
   end
 
   # create action
   test 'create with valid parameters should create an api key' do
-    @controller.expects(:current_user).at_least_once.returns(accounts(:user))
+    login_as accounts(:user)
     post :create, account_id: accounts(:user).id, api_key: { name: 'Name',
                                                              description: 'It was the best of times.',
                                                              terms: '1' }
@@ -222,7 +222,7 @@ class ApiKeysControllerTest < ActionController::TestCase
   end
 
   test 'create requires accepting the terms of service' do
-    @controller.expects(:current_user).at_least_once.returns(accounts(:user))
+    login_as accounts(:user)
     post :create, account_id: accounts(:user).id, api_key: { name: 'Name',
                                                              description: 'I do not accept those terms!',
                                                              terms: '0' }
@@ -234,14 +234,14 @@ class ApiKeysControllerTest < ActionController::TestCase
   # edit action
   test 'edit should populate the form' do
     api_key = create(:api_key, account_id: accounts(:user).id, description: 'A pre-existing API Key.')
-    @controller.expects(:current_user).at_least_once.returns(accounts(:user))
+    login_as accounts(:user)
     get :edit, account_id: accounts(:user).id, id: api_key.id
     assert_response :ok
     assert response.body.match('A pre-existing API Key.')
   end
 
   test 'edit should 404 attempting to edit a non-existant api key' do
-    @controller.expects(:current_user).at_least_once.returns(accounts(:user))
+    login_as accounts(:user)
     get :edit, account_id: accounts(:user).id, id: 9876
     assert_response :not_found
   end
@@ -249,7 +249,7 @@ class ApiKeysControllerTest < ActionController::TestCase
   # update action
   test 'update should populate the form' do
     api_key = create(:api_key, account_id: accounts(:user).id, description: 'My old crufty API Key.')
-    @controller.expects(:current_user).at_least_once.returns(accounts(:user))
+    login_as accounts(:user)
     put :update, account_id: accounts(:user).id, id: api_key.id, api_key: { name: 'Name',
                                                                             description: 'Repolished key!',
                                                                             terms: '1' }
@@ -260,7 +260,7 @@ class ApiKeysControllerTest < ActionController::TestCase
 
   test 'update does not allow unaccepting the terms' do
     api_key = create(:api_key, account_id: accounts(:user).id, description: 'My previous API Key.')
-    @controller.expects(:current_user).at_least_once.returns(accounts(:user))
+    login_as accounts(:user)
     put :update, account_id: accounts(:user).id, id: api_key.id, api_key: { name: 'Name',
                                                                             description: 'I cleverly unaccept now!',
                                                                             terms: '0' }
@@ -272,7 +272,7 @@ class ApiKeysControllerTest < ActionController::TestCase
   # destroy action
   test 'destroy should remove the api key from the db' do
     api_key = create(:api_key, account_id: accounts(:user).id, description: 'My doomed key.')
-    @controller.expects(:current_user).at_least_once.returns(accounts(:user))
+    login_as accounts(:user)
     delete :destroy, account_id: accounts(:user).id, id: api_key.id
     assert_response 302
     assert !ApiKey.where(account_id: accounts(:user).id, description: 'My doomed key.').first
