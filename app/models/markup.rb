a6044491fca427f66d7cc9a8b75132145c2af655
bobofraggins <peter.fry@mac.com>
2014-12-12 10:01:31 -0500

    OTWO-3293 Implement Markup model. As it is only used in a single place (account.rb: about_markup), we can get rid of 90% of the code. Implement filtering of HTML tags from code in a way that does not load view helpers at the model layer. Rejigger the test load order to make SimpleCov include files required by the environment.rb.

--- app/models/markup.rb
+++ app/models/markup.rb
@@ -1,2 +1,9 @@
 class Markup < ActiveRecord::Base
+  before_save :sanitize_html
+
+  private
+
+  def sanitize_html
+    self.formatted = raw.strip_tags_preserve_line_breaks
+  end
 end
---
+++ config/initializers/core_extensions.rb
@@ -0,0 +1,1 @@
+Dir["#{Rails.root}/app/core_extensions/**/*.rb"].each { |file| require file }
