bad7befdc9a03eab176c25dfc330189741f803e9
Paddy <rapbhan@gmail.com>
2015-01-08 19:02:01 +0530

    OTWO-3298 Fixed the Pagination w.r.t Bootstrap3

---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,19 @@
+module WillPaginate
+  module ActionView
+    def will_paginate(collection = nil, options = {})
+      options[:renderer] ||= BootstrapPagination::Rails
+      options[:previous_label] = "&larr;"
+      options[:next_label] = "&rarr;"
+      super.try :html_safe
+    end
+  end
+end
+
+module BootstrapPagination
+  class Rails < WillPaginate::ActionView::LinkRenderer
+    def to_html
+      ul_element = super
+      tag(:div, ul_element, class: "oh_pagination text-center")
+    end
+  end
+end
