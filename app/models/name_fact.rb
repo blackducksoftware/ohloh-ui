46050a61bcf9c342a8aa0aef09b00264007e9e13
Harish Natarajan <harish.natarajan@imaginea.com>
2015-01-08 10:59:42 +0530

    OTWO-3298 Seriliazation added for required attrs in name_fact.rb

--- app/models/name_fact.rb
+++ app/models/name_fact.rb
@@ -1,5 +1,7 @@
 class NameFact < ActiveRecord::Base
   include Comparable
+  serialize  :commits_by_project
+  serialize  :commits_by_language
 
   belongs_to :name
   has_one :project, -> { where { deleted.not_eq(true) } }, foreign_key: :best_analysis_id, primary_key: :analysis_id
