bd238f5d1e80ee8d2614a296e5eab774a83cb8d9
Alex Johnson <notalexjohnson@gmail.com>
2015-01-12 17:44:42 +0530

    Avoid abbreviated method names

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -2,11 +2,12 @@ class EmailAddress < ActiveRecord::Base
   BLACKLISTED_EMAILS = ['root@localhost']
 
   class << self
-    def search_sql(address_str)
-      conds = ['address IN (?)', address_str.split - BLACKLISTED_EMAILS]
+    def search_sql(address_string)
+      conditions = ['address IN (?)', address_string.split - BLACKLISTED_EMAILS]
+
       %[SELECT array_agg(id) AS ids
       FROM email_addresses
-      WHERE #{sanitize_sql_array(conds)}]
+      WHERE #{ sanitize_sql_array(conditions) }]
     end
   end
 end
