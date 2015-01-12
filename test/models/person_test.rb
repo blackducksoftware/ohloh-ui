4a9611c49b0e3043f4a01aacadb76659bc9a57da
Harish Natarajan <harish.natarajan@imaginea.com>
2015-01-12 17:19:54 +0530

    OTWO-3298 Fixed tests for person model

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,15 +1,20 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  let(:admin_account) { accounts(:admin) }
-  let(:linux_project) { projects(:linux) }
-
   it 'should return claimed persons when availabale when page is not given' do
-    assert_equal 7, Person.claimed.length
+    claimed_persons = Person.claimed
+
+    claimed_persons.length.must_equal 7
+    account_ids = claimed_persons.map(&:account_id).compact
+    account_ids.size.must_equal 7
   end
 
   it 'should return claimed persons when availabale when page is 2' do
     Person.stubs(:per_page).returns(5)
-    assert_equal 2, Person.claimed(2).length
+    claimed_persons = Person.claimed(2)
+
+    claimed_persons.length.must_equal 2
+    account_ids = claimed_persons.map(&:account_id).compact
+    account_ids.size.must_equal 2
   end
 end
