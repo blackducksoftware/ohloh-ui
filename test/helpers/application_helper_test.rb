0cf6a2357b0edc238dbf9eb9b8bc8ff10c67ce84
Harish Natarajan <harish.natarajan@imaginea.com>
2015-01-22 13:17:37 +0530

    OTWO=3315 Added test for new application helper

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -70,4 +70,14 @@ class ApplicationHelperTest < ActionView::TestCase
       generate_page_name.must_equal 'accounts_index_page'
     end
   end
+
+  describe 'months_in_range' do
+    it 'should return months in between given range of dates' do
+      dates = months_in_range(Date.today - 2.months, Date.today)
+      dates.size.must_equal 3
+      dates.first.must_equal((Date.today - 2.months).beginning_of_month)
+      dates[1].must_equal((Date.today - 1.month).beginning_of_month)
+      dates.last.must_equal(Date.today.beginning_of_month)
+    end
+  end
 end
