3aec0aa0b8863d6fa8e095b2afa1247cff6d84e9
Harish Natarajan <harish.natarajan@imaginea.com>
2015-01-22 13:05:40 +0530

    OTWO-3315 Added explanatory comment in position decorator

--- app/decorators/position_decorator.rb
+++ app/decorators/position_decorator.rb
@@ -1,6 +1,7 @@
 class PositionDecorator < Draper::Decorator
   delegate_all
 
+  # TODO: Replaces position_analyzed in account/reports.rb
   def analyzed?
     cbp = account.decorate.symbolized_commits_by_project.index_by { |c| c[:position_id].to_i }
     cbp[id].present?
