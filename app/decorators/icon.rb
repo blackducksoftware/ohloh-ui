dac17984e44498a99a2a71cb5c5e13491b4107c1
Harish Natarajan <harish.natarajan@imaginea.com>
2015-02-05 14:28:07 +0530

    OTWO-3314 Fixed typo in icon.rb

--- app/decorators/icon.rb
+++ app/decorators/icon.rb
@@ -29,12 +29,12 @@ class Icon < Draper::Decorator
   end
 
   def default_style
-    fnt_size = FONT_SIZES[int_size] || 14
+    font_size = FONT_SIZES[int_size] || 14
     @opts.reverse_merge!(bg: 'EEE', color: '000')
     margin_right = int_size == 64 ? 0 : 2
 
     "background-color:##{@opts[:bg]}; color:##{@opts[:color]}; border:1px dashed ##{@opts[:color]};"\
-    "font-size:#{fnt_size}px; line-height:#{int_size}px; #{dimensions}"\
+    "font-size:#{font_size}px; line-height:#{int_size}px; #{dimensions}"\
     "text-align:center; float:left; margin-bottom:0; margin-top:0; margin-right:#{margin_right}px"
   end
 end
