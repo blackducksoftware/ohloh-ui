cb895202a5f8adf0d083acbdf2ea02efab305d2f
Harish Natarajan <harish.natarajan@imaginea.com>
2015-02-12 19:39:23 +0530

    OTWO-3314 Refactored vita_language_facts_test.rb

--- test/factories/vita_language_facts.rb
+++ test/factories/vita_language_facts.rb
@@ -2,5 +2,8 @@ FactoryGirl.define do
   factory :vita_language_fact do
     association :vita, factory: :best_vita
     association :language
+    total_months 10
+    total_commits 10
+    total_activity_lines 10
   end
 end
--- test/models/vita_language_facts_test.rb
+++ test/models/vita_language_facts_test.rb
@@ -9,12 +9,12 @@ class VitaLanguageFactTest < ActiveSupport::TestCase
       lang4 = create(:language, category: 1)
       lang5 = create(:language, category: 2)
       lang6 = create(:language, category: 2)
-      fact1 = create(:vita_language_fact, language: lang1, total_months: 10, total_commits: 10, total_activity_lines: 10)
-      fact2 = create(:vita_language_fact, language: lang2, total_months: 10, total_commits: 10, total_activity_lines: 20)
-      fact3 = create(:vita_language_fact, language: lang3, total_months: 10, total_commits: 100, total_activity_lines: 10)
-      fact4 = create(:vita_language_fact, language: lang4, total_months: 10, total_commits: 10, total_activity_lines: 100)
-      fact5 = create(:vita_language_fact, language: lang5, total_months: 10, total_commits: 100, total_activity_lines: 10)
-      fact6 = create(:vita_language_fact, language: lang6, total_months: 50, total_commits: 10, total_activity_lines: 10)
+      fact1 = create(:vita_language_fact, language: lang1)
+      fact2 = create(:vita_language_fact, language: lang2, total_activity_lines: 20)
+      fact3 = create(:vita_language_fact, language: lang3, total_commits: 100)
+      fact4 = create(:vita_language_fact, language: lang4, total_activity_lines: 100)
+      fact5 = create(:vita_language_fact, language: lang5, total_commits: 100)
+      fact6 = create(:vita_language_fact, language: lang6, total_months: 50)
 
       ordered_facts = VitaLanguageFact.where.not(language_id: nil).ordered
       ordered_facts.map(&:id).must_equal [fact2.id, fact1.id, fact3.id, fact4.id, fact6.id, fact5.id]
