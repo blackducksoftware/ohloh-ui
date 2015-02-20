b801a593905ac5350ac505859678e60d0ddafcea
bobofraggins <peter.fry@mac.com>
2015-02-20 10:54:50 -0500

    Implement organizations#affiliated_committers

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -12,4 +12,27 @@ module OrganizationsHelper
       concat " (#{percentage.floor}%)" if previous > 0
     end
   end
+
+  def organization_affiliated_committers_stats(account_stat)
+    return false unless account_stat
+    most_commit_stat = org_most_commit_stat(account_stat)
+    most_recent_stat = org_most_recent_stat(account_stat)
+    return false unless most_commit_stat['project_id'] && most_recent_stat['project_id']
+    {
+      most_committed_project: Project.find(most_commit_stat['project_id']),
+      most_recent_project: Project.find(most_recent_stat['project_id']),
+      max_commits: most_commit_stat['max_commits'],
+      last_checkin: most_recent_stat['last_checkin']
+    }
+  end
+
+  private
+
+  def org_most_commit_stat(account_stat)
+    account_stat.sort_by { |hsh| hsh['max_commits'].to_i }.reverse.first || {}
+  end
+
+  def org_most_recent_stat(account_stat)
+    account_stat.sort_by { |hsh| hsh['last_checkin'] }.reverse.first || {}
+  end
 end
