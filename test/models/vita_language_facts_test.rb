e2cb0e585ca4c9bc1647ca43696014e5d89d35aa
Harish Natarajan <harish.natarajan@imaginea.com>
2015-02-23 18:00:14 +0530

    OTWO-3342 Added tests for vita language facts

--- test/models/vita_language_facts_test.rb
+++ test/models/vita_language_facts_test.rb
@@ -1,23 +1,32 @@
 require 'test_helper'
 
 class VitaLanguageFactTest < ActiveSupport::TestCase
+  let(:lang1) { create(:language, category: 0) }
+  let(:lang2) { create(:language, category: 0) }
+  let(:lang3) { create(:language, category: 1) }
+  let(:lang4) { create(:language, category: 1) }
+  let(:lang5) { create(:language, category: 2) }
+  let(:lang6) { create(:language, category: 2) }
+  let(:fact1) { create(:vita_language_fact, language: lang1, most_commits: 600) }
+  let(:fact2) { create(:vita_language_fact, language: lang2, total_activity_lines: 20, most_commits: 100) }
+  let(:fact3) { create(:vita_language_fact, language: lang3, total_commits: 100, most_commits: 200) }
+  let(:fact4) { create(:vita_language_fact, language: lang4, total_activity_lines: 100, most_commits: 300) }
+  let(:fact5) { create(:vita_language_fact, language: lang5, total_commits: 100, most_commits: 400) }
+  let(:fact6) { create(:vita_language_fact, language: lang6, total_months: 50, most_commits: 500) }
+
   describe 'ordered' do
     it 'should return ordered results' do
-      lang1 = create(:language, category: 0)
-      lang2 = create(:language, category: 0)
-      lang3 = create(:language, category: 1)
-      lang4 = create(:language, category: 1)
-      lang5 = create(:language, category: 2)
-      lang6 = create(:language, category: 2)
-      fact1 = create(:vita_language_fact, language: lang1)
-      fact2 = create(:vita_language_fact, language: lang2, total_activity_lines: 20)
-      fact3 = create(:vita_language_fact, language: lang3, total_commits: 100)
-      fact4 = create(:vita_language_fact, language: lang4, total_activity_lines: 100)
-      fact5 = create(:vita_language_fact, language: lang5, total_commits: 100)
-      fact6 = create(:vita_language_fact, language: lang6, total_months: 50)
-
+      result = [fact2.id, fact1.id, fact3.id, fact4.id, fact6.id, fact5.id]
       ordered_facts = VitaLanguageFact.where.not(language_id: nil).ordered
-      ordered_facts.map(&:id).must_equal [fact2.id, fact1.id, fact3.id, fact4.id, fact6.id, fact5.id]
+      ordered_facts.map(&:id).must_equal result
+    end
+  end
+
+  describe 'with_languages_and_commits' do
+    it 'should return ordered by most commits' do
+      result = [fact1.id, fact6.id, fact5.id, fact4.id, fact3.id, fact2.id]
+      ordered_facts = VitaLanguageFact.where.not(language_id: nil).with_languages_and_commits
+      ordered_facts.map(&:id).must_equal result
     end
   end
 end
