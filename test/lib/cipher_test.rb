f483f43b98979a32fdf605bb3939e000f86b2720
Harish Natarajan <harish.natarajan@imaginea.com>
2015-02-23 21:08:50 +0530

    OTWO-3342 Added test for cipher.rb

--- test/lib/cipher_test.rb
+++ test/lib/cipher_test.rb
@@ -5,9 +5,18 @@ class CipherTest < ActiveSupport::TestCase
     it 'should encrypt and given data correctly' do
       encrypted_data = Ohloh::Cipher.encrypt('robin')
       encrypted_data.wont_equal 'robin'
+    end
+  end
 
+  describe 'decrypt' do
+    it 'should encrypt and given data correctly' do
+      encrypted_data = Ohloh::Cipher.encrypt('robin')
       decrypted_data = Ohloh::Cipher.decrypt(encrypted_data)
       decrypted_data.must_equal 'robin'
+    end
+
+    it 'should return nil if error is thrown' do
+      Ohloh::Cipher.decrypt('robin', 'junk_key')
     end
   end
 end
