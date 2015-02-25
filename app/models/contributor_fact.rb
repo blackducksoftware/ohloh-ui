dbd618182cd904be22f910290aa9febc56ad5ec3
bobofraggins <peter.fry@mac.com>
2015-02-25 11:31:08 -0500

    Use AREL instead of inline sql.

--- app/models/contributor_fact.rb
+++ app/models/contributor_fact.rb
@@ -18,15 +18,9 @@ class ContributorFact < NameFact
     end
 
     def first_for_name_id_and_project_id(name_id, project_id)
-      sql = <<-SQL
-        SELECT name_facts.* FROM name_facts
-          INNER JOIN projects ON projects.best_analysis_id = name_facts.analysis_id
-          WHERE ( name_facts.name_id = #{name_id.to_i} OR name_facts.name_id IN (
-            SELECT AA.preferred_name_id FROM analysis_aliases AA
-            WHERE AA.analysis_id = projects.best_analysis_id AND AA.commit_name_id = #{name_id.to_i}))
-          AND projects.id = #{project_id.to_i} AND type='ContributorFact'
-        SQL
-      ContributorFact.find_by_sql(sql).first
+      ContributorFact.joins(:project).where(projects: { id: project_id })
+        .where('name_id = ? or name_id in (?)', name_id,
+               AnalysisAlias.select(:preferred_name_id).joins(:project).where(commit_name_id: name_id)).first
     end
   end
 end
