4317a5620daea77f76903041e38bb3f6f2a3a8b3
santhanakarthikeyan <santhanakarthikeyan.s@imaginea.com>
2015-03-04 17:58:25 +0530

    OTWO-3390: Migrate activation resends controller

---
+++ app/mailers/account_mailer.rb
@@ -0,0 +1,9 @@
+class AccountMailer < ActionMailer::Base
+  default from: 'mailer@openhub.net'
+
+  def signup_notification(account)
+    @account = account
+    @url = activate_account_url(id: account.to_param, code: account.activation_code, host: URL_HOST, protocol: 'https')
+    mail to: account.email, subject: t('.subject'), bcc: 'pdegenportnoy@blackducksoftware.com'
+  end
+end
