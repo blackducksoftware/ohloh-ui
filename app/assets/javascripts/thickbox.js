42573e0932df5e6bd179b2f652b9937cbaf1cc58
Paddy <rapbhan@gmail.com>
2015-03-10 14:09:04 +0530

    OTWO-3367 Fixed the Image path issue on ThickBox.js

--- app/assets/javascripts/thickbox.js
+++ app/assets/javascripts/thickbox.js
@@ -5,7 +5,7 @@
  * Licensed under the MIT License: http://www.opensource.org/licenses/mit-license.php
 */
 
-var tb_pathToImage = "/assets/ui/loadingAnimation.gif";
+//var tb_pathToImage = "/assets/ui/loadingAnimation.gif";
 
 /*!!!!!!!!!!!!!!!!! edit below this line at your own risk !!!!!!!!!!!!!!!!!!!!!!!*/
 
