2b41a1aeca80199af72dc7746f3b00a2874997e5
bobofraggins <peter.fry@mac.com>
2015-03-13 11:10:50 -0400

    Patch some missing code coverage.

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -1,94 +1,121 @@
 require 'test_helper'
 
 class ApplicationControllerTest < ActionController::TestCase
-  setup do
-    @controller = TestController.new
-    @controller.request = @request
-    @controller.response = @response
+  describe 'TestController' do
+    setup do
+      @controller = TestController.new
+      @controller.request = @request
+      @controller.response = @response
+    end
+
+    it 'render_404 as html' do
+      get :renders_404
+      must_respond_with :not_found
+      response.body.must_include(I18n.t(:four_oh_four))
+      response.headers['Content-Type'].must_include('text/html')
+    end
+
+    it 'render_404 as json' do
+      get :renders_404, format: 'json'
+      must_respond_with :not_found
+      response.body.must_include(I18n.t(:four_oh_four))
+      response.headers['Content-Type'].must_include('application/json')
+    end
+
+    it 'render_404 as xml' do
+      get :renders_404, format: 'xml'
+      must_respond_with :not_found
+      response.body.must_include(I18n.t(:four_oh_four))
+      response.headers['Content-Type'].must_include('application/xml')
+    end
+
+    it 'error message as html' do
+      get :error_with_message
+      must_respond_with :unauthorized
+      response.body.must_include('test error string')
+    end
+
+    it 'error message as json' do
+      get :error_with_message, format: 'json'
+      must_respond_with :unauthorized
+      response.body.must_include('test error string')
+    end
+
+    it 'error message as xml' do
+      get :error_with_message, format: 'xml'
+      must_respond_with :unauthorized
+      response.body.must_include('test error string')
+    end
+
+    it 'session_required with a current user' do
+      login_as create(:account)
+      get :session_required_action
+      must_respond_with :ok
+    end
+
+    it 'session_required without a current user' do
+      login_as nil
+      get :session_required_action
+      must_respond_with :unauthorized
+    end
+
+    it 'admin_session_required with a current admin' do
+      login_as create(:admin)
+      get :admin_session_required_action
+      must_respond_with :ok
+    end
+
+    it 'admin_session_required without a current user' do
+      login_as nil
+      get :admin_session_required_action
+      must_respond_with :unauthorized
+    end
+
+    it 'admin_session_required with a current plain user' do
+      login_as create(:account)
+      get :admin_session_required_action
+      must_respond_with :unauthorized
+    end
+
+    it 'ParamRecordNotFound exceptions are caught and not passed on as 500s' do
+      get :throws_param_record_not_found
+      must_respond_with :not_found
+    end
+
+    it 'remember me functionality automatically logs users in' do
+      login_as nil
+      admin = create(:admin)
+      Account::Authenticator.remember(admin)
+      @request.cookies[:auth_token] = admin.remember_token
+      get :session_required_action
+      must_respond_with :ok
+      session[:account_id].must_equal admin.id
+    end
   end
 
-  it 'render_404 as html' do
-    get :renders_404
-    must_respond_with :not_found
-    response.body.must_include(I18n.t(:four_oh_four))
-    response.headers['Content-Type'].must_include('text/html')
-  end
+  describe 'ProjectsController' do
+    setup do
+      @controller = ProjectsController.new
+      @controller.request = @request
+      @controller.response = @response
+    end
 
-  it 'render_404 as json' do
-    get :renders_404, format: 'json'
-    must_respond_with :not_found
-    response.body.must_include(I18n.t(:four_oh_four))
-    response.headers['Content-Type'].must_include('application/json')
-  end
+    it 'clears reminders' do
+      user = create(:account)
+      project = create(:project)
+      action = user.actions.create!(status: Action::STATUSES[:remind], stack_project: project)
 
-  it 'render_404 as xml' do
-    get :renders_404, format: 'xml'
-    must_respond_with :not_found
-    response.body.must_include(I18n.t(:four_oh_four))
-    response.headers['Content-Type'].must_include('application/xml')
-  end
+      login_as user
+      get :show, id: create(:project).to_param
+      @response.body.must_match(/You can add more projects now./)
+      assert_response :success
+      action.reload.status.must_equal Action::STATUSES[:remind]
 
-  it 'error message as html' do
-    get :error_with_message
-    must_respond_with :unauthorized
-    response.body.must_include('test error string')
-  end
-
-  it 'error message as json' do
-    get :error_with_message, format: 'json'
-    must_respond_with :unauthorized
-    response.body.must_include('test error string')
-  end
-
-  it 'error message as xml' do
-    get :error_with_message, format: 'xml'
-    must_respond_with :unauthorized
-    response.body.must_include('test error string')
-  end
-
-  it 'session_required with a current user' do
-    login_as create(:account)
-    get :session_required_action
-    must_respond_with :ok
-  end
-
-  it 'session_required without a current user' do
-    login_as nil
-    get :session_required_action
-    must_respond_with :unauthorized
-  end
-
-  it 'admin_session_required with a current admin' do
-    login_as create(:admin)
-    get :admin_session_required_action
-    must_respond_with :ok
-  end
-
-  it 'admin_session_required without a current user' do
-    login_as nil
-    get :admin_session_required_action
-    must_respond_with :unauthorized
-  end
-
-  it 'admin_session_required with a current plain user' do
-    login_as create(:account)
-    get :admin_session_required_action
-    must_respond_with :unauthorized
-  end
-
-  it 'ParamRecordNotFound exceptions are caught and not passed on as 500s' do
-    get :throws_param_record_not_found
-    must_respond_with :not_found
-  end
-
-  it 'remember me functionality automatically logs users in' do
-    login_as nil
-    admin = create(:admin)
-    Account::Authenticator.remember(admin)
-    @request.cookies[:auth_token] = admin.remember_token
-    get :session_required_action
-    must_respond_with :ok
-    session[:account_id].must_equal admin.id
+      get :show, id: create(:project).url_name, clear_action_reminder: action.id
+      @response.body.wont_match(/You can add more projects now./)
+      assert_response :success
+      action.reload.status.must_equal Action::STATUSES[:completed]
+    end
   end
 end
 
