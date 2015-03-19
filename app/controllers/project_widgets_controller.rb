f4e8f00885ff058523a96de34db5d8cc29ea854c
Harish Natarajan <harish.natarajan@imaginea.com>
2015-03-19 17:18:08 +0530

    OTWO-3387 Added project widgets controller

---
+++ app/controllers/project_widgets_controller.rb
@@ -0,0 +1,16 @@
+class ProjectWidgetsController < WidgetsController
+  before_action :set_project
+  before_action :render_gif_image, only: [:partner_badge, :thin_badge]
+  before_action :render_not_supported_thin_badge, except: [:partner_badge, :thin_badge, :index]
+  before_action :render_for_js_format
+
+  def index
+    @widgets = ProjectWidget.create_widgets(params[:project_id])
+  end
+
+  private
+
+  def set_project
+    @project = Project.from_param(params[:project_id]).first
+  end
+end
