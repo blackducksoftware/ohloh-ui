ce5defc8a18e8824c9595734e109147b90c5071c
Harish Natarajan <harish.natarajan@imaginea.com>
2015-03-21 13:53:04 +0530

    OTWO-3387 Added test for org widget model

---
+++ test/models/widget/organization_widget_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class OrganizationWidgetTest < ActiveSupport::TestCase
+  let(:org) { create(:organization) }
+  let(:widget) { OrganizationWidget.new(organization_id: org.id) }
+
+  describe 'title' do
+    it 'should return title' do
+      widget.title.must_equal I18n.t('organization_widgets.title')
+    end
+  end
+
+  describe 'border' do
+    it 'should return zero' do
+      widget.border.must_equal 0
+    end
+  end
+
+  describe 'organization' do
+    it 'should return organization' do
+      widget.organization.must_equal org
+    end
+  end
+
+  describe 'height' do
+    it 'should return 200' do
+      widget.height.must_equal 200
+    end
+  end
+
+  describe 'width' do
+    it 'should return 328' do
+      widget.width.must_equal 328
+    end
+  end
+
+  describe 'create_widgets' do
+    it 'should create descendan widgets' do
+      widget_classes = [OrganizationWidget::OpenSourceActivity,
+                        OrganizationWidget::AffiliatedCommittersActivity,
+                        OrganizationWidget::PortfolioProjectsActivity]
+      OrganizationWidget.create_widgets(org.id).map(&:class).must_equal widget_classes
+    end
+  end
+
+  describe 'initialize' do
+    it 'should raise exception if account is missing' do
+      proc { OrganizationWidget.new }.must_raise ArgumentError
+    end
+  end
+end
