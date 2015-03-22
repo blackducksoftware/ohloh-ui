40219ad9cda6cf46f3d92d859a4be0a4b805342d
Harish Natarajan <harish.natarajan@imaginea.com>
2015-03-22 18:19:47 +0530

    OTWO-3387 Refactored stack widgets view

--- app/controllers/stack_widgets_controller.rb
+++ app/controllers/stack_widgets_controller.rb
@@ -2,16 +2,14 @@ class StackWidgetsController < WidgetsController
   before_action :set_stack_and_widget
   before_action :render_not_supported_thin_badge
   before_action :render_for_js_format
+  before_action :account_context, only: :index
   skip_before_action :set_widget
-
-  def index
-    @account = @stack.account
-  end
 
   private
 
   def set_stack_and_widget
     @widget = StackWidget.new(stack_id: params[:stack_id])
     @stack = Stack.where(id: params[:stack_id]).first!
+    @account = @stack.account
   end
 end
