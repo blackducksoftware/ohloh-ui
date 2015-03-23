2f3281983a7fe84eb4b7df417dda60f961800494
Harish Natarajan <harish.natarajan@imaginea.com>
2015-03-23 15:33:21 +0530

    OTWO-3387 Fixed org widgets index page

--- app/controllers/organization_widgets_controller.rb
+++ app/controllers/organization_widgets_controller.rb
@@ -2,6 +2,7 @@ class OrganizationWidgetsController < WidgetsController
   before_action :set_organization
   before_action :render_not_supported_thin_badge
   before_action :render_for_js_format
+  before_action :organization_context, only: :index
 
   def index
     @widgets = OrganizationWidget.create_widgets(params[:organization_id])
