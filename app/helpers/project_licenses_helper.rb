6deeabfafd65ee31d5add0395e023cec4c97f5b6
bobofraggins <peter.fry@mac.com>
2015-03-24 16:01:15 -0400

    Add tests for the project_licenses route. Fix disabled_button.

--- app/helpers/project_licenses_helper.rb
+++ app/helpers/project_licenses_helper.rb
@@ -1,9 +1,10 @@
 module ProjectLicensesHelper
   def show_license_button(project)
+    button_class = 'btn btn-primary new-license'
     if project.edit_authorized?
-      link_to t('project_licenses.new_declared_license'), new_project_license_path(project), class: 'btn btn-primary'
+      link_to t('project_licenses.new_declared_license'), new_project_license_path(project), class: button_class
     else
-      disabled_button t('project_licenses.new_declared_license'), class: 'btn-primary'
+      disabled_button t('project_licenses.new_declared_license'), class: button_class
     end
   end
 end
