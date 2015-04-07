205e1e3f6cc2f3d4ebaade3ac669bb84f14ed6a0
Harish Natarajan <harish.natarajan@imaginea.com>
2015-04-07 10:42:31 +0530

    OTWO-3359 Implemented contributors autocomplete action

--- app/controllers/autocompletes_controller.rb
+++ app/controllers/autocompletes_controller.rb
@@ -1,4 +1,6 @@
 class AutocompletesController < ApplicationController
+  before_action :check_for_project, only: :contributors
+
   # NOTE: Replaces accounts#autocomplete,
   def account
     accounts = Account.simple_search(params[:term])
@@ -19,7 +21,22 @@ class AutocompletesController < ApplicationController
     render text: licenses.map { |l| { nice_name: l.nice_name, id: l.id.to_s } }.to_json
   end
 
-  # NOTE: Replaces contibutors#autocomplete
+  # NOTE: Replaces contributors#autocomplete
   def contributors
+    search_term = "%#{params[:term].strip.downcase}%"
+    name_facts = NameFact.where(analysis_id: project.best_analysis_id)
+                  .where(['lower(names.name) like ? ', search_term])
+                  .includes(:name).references(:all).order('names.name ASC').limit(10)
+    render json: name_facts.map { |nf| nf.name.name }
+  end
+
+  private
+
+  def check_for_project
+    project_name = params[:project].to_s.strip.downcase
+    render text: '' && return if project_name.empty?
+
+    project = Project.active.where(['lower(name) = ?', project_name]).first
+    render text: '' && return if project.nil? || project.best_analysis_id.nil?
   end
 end
