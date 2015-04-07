6fa3017253fd461719591bd3e171d8cd4f173e6f
Harish Natarajan <harish.natarajan@imaginea.com>
2015-04-07 22:23:35 +0530

    OTWO-3359 Added tests for nil contributor fact

---
+++ test/null_models/nil_contributor_fact_test.rb
@@ -0,0 +1,59 @@
+require 'test_helper'
+
+class NilContributorFactTest < ActiveSupport::TestCase
+  let(:nil_contributor_fact) { NilContributorFact.new }
+
+  describe 'name_id' do
+    it 'should return nil' do
+      nil_contributor_fact.name_id.must_equal nil
+    end
+  end
+
+  describe 'primary_language' do
+    it 'should return nil' do
+      nil_contributor_fact.primary_language.must_equal nil
+    end
+  end
+
+  describe 'first_checkin' do
+    it 'should return nil' do
+      nil_contributor_fact.first_checkin.must_equal nil
+    end
+  end
+
+  describe 'last_checkin' do
+    it 'should return nil' do
+      nil_contributor_fact.last_checkin.must_equal nil
+    end
+  end
+
+  describe 'twelve_month_commits' do
+    it 'should return zero' do
+      nil_contributor_fact.twelve_month_commits.must_equal 0
+    end
+  end
+
+  describe 'commits' do
+    it 'should return zero' do
+      nil_contributor_fact.commits.must_equal 0
+    end
+  end
+
+  describe 'nil?' do
+    it 'should return true' do
+      nil_contributor_fact.nil?.must_equal true
+    end
+  end
+
+  describe 'blank?' do
+    it 'should return true' do
+      nil_contributor_fact.blank?.must_equal true
+    end
+  end
+
+  describe 'present?' do
+    it 'should be false' do
+      nil_contributor_fact.present?.must_equal false
+    end
+  end
+end
