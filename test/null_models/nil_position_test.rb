3092258187b91c5da356f531d97259e2e8c82e31
Harish Natarajan <harish.natarajan@imaginea.com>
2015-04-07 22:12:27 +0530

    OTWO-3359 Added tests for nil position

---
+++ test/null_models/nil_position_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class NilPositionTest < ActiveSupport::TestCase
+  let(:nil_position) { NilPosition.new }
+
+  describe 'title' do
+    it 'should return nil' do
+      nil_position.title.must_equal nil
+    end
+  end
+
+  describe 'active?' do
+    it 'should return false' do
+      nil_position.active?.must_equal false
+    end
+  end
+
+  describe 'nil?' do
+    it 'should return true' do
+      nil_position.nil?.must_equal true
+    end
+  end
+
+  describe 'blank?' do
+    it 'should return true' do
+      nil_position.blank?.must_equal true
+    end
+  end
+
+  describe 'present?' do
+    it 'should be false' do
+      nil_position.present?.must_equal false
+    end
+  end
+end
