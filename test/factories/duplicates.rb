82a043a4fc62241608a739b8f69bd4d994a0124e
bobofraggins <peter.fry@mac.com>
2015-04-14 13:08:29 -0400

    Implement duplicates model.

---
+++ test/factories/duplicates.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :duplicate do
+    association :account
+    association :good_project, factory: :project
+    association :bad_project, factory: :project
+    comment { Faker::Lorem.sentence }
+  end
+end
