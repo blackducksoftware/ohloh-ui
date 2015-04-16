3f1764e2290ef0d8e9a0b159438038312f81225a
Harish Natarajan <harish.natarajan@imaginea.com>
2015-04-16 17:22:44 +0530

    OTWO-3446 Fixed commits spark action in analyses controller

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -48,7 +48,7 @@ class AnalysesController < ApplicationController
   end
 
   def commits_spark
-    spark_image = NewSpark.new(@analysis.monthly_commits, max_value: 5000).render.to_blob
+    spark_image = Spark::SimpleSpark.new(@analysis.monthly_commits, max_value: 5000).render.to_blob
     send_data spark_image, type: 'image/png', filename: 'commits.png', disposition: 'inline'
   end
 
