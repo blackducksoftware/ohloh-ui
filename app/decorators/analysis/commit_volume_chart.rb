69271621cb2380d61b396ed9f3e9e031be1d55ac
Harish Natarajan <harish.natarajan@imaginea.com>
2015-04-19 19:12:30 +0530

    OTWO-3446 Refactored commit_volume_chart class

--- app/decorators/analysis/commit_volume_chart.rb
+++ app/decorators/analysis/commit_volume_chart.rb
@@ -1,82 +1,59 @@
 class Analysis::CommitVolumeChart
-  DEFAULT_NAME_COUNT = 5
-  INTERVALS = ['50 years', '12 months', '1 month']
   OTHER = 'Other'
+  NAME_COUNT = 5
+  INTERVALS = ['50 years', '12 months', '1 month']
 
   def initialize(analysis)
     @analysis = analysis
-    @history = Analysis::CommitVolume.new(analysis)
+    history_for_all_intervals
+    @interval_labels = ['All Time', 'Past 12 Months', @analysis.max_month.strftime('%B %Y')]
   end
 
   def data
     COMMIT_VOLUME_CHART_DEFAULTS.merge(
       'series' => pivoted_series.map { |name, data| { 'name' => name, 'data' => data } },
-      'xAxis' => { 'categories' => interval_labels },
-      'warning' => warning_message(series[1])
+      'xAxis' => { 'categories' => @interval_labels },
+      'warning' => warning_messages
     )
   end
 
   private
 
-  def regroup(top_names, a)
-    result = []
-    other_count = 0
-
-    a.each do |name, count|
-      if top_names.include? name
-        result << [name, count]
-      else
-        other_count += count
-      end
+  def history_for_all_intervals
+    @history = INTERVALS.map do |interval|
+      Analysis::CommitVolume.new(@analysis, interval).collection
     end
-
-    result << [OTHER, other_count] if other_count > 0
-    result
-  end
-
-  def interval_labels
-    ['All Time', 'Past 12 Months', @analysis.max_month.strftime('%B %Y')]
   end
 
   def series
-    @series ||= {}
-    n = DEFAULT_NAME_COUNT
-    @series[n] ||= begin
-      queries = INTERVALS.map { |interval| commits_by_name(interval) }
-      top_names = queries.map { |s| s[0..(n-1)].map(&:first) }.flatten.sort.uniq
-      queries.map { |s| regroup(top_names, s) }
+    @series ||= @history.map do |data|
+      others_count = data.drop(NAME_COUNT).map(&:last).sum
+      data.take(NAME_COUNT) + [OTHER, others_count]
     end
   end
 
   def pivoted_series
-    @pivoted_series ||= {}
-    @pivoted_series[DEFAULT_NAME_COUNT] ||= begin
-       all_names = []
-       series.each do |s|
-         s.each do |name, count|
-           all_names << name unless all_names.include? name
-         end
-       end
-
-       result = all_names.map { |name| [name, []] }
-
-       series.each do |s|
-         all_names.each_with_index do |name, i|
-           n, count = s.find { |n, c| n == name }
-           result[i][1] << count.to_i
-         end
-       end
-       result
-     end
+    @pivoted_series ||= committer_names.map { |name| [name, find_count(name)] }
   end
 
-  def warning_message(s)
-    half = s.map{ |name, count| count }.sum / 2
-    s.each do |name, count|
-      if count > half and name != OTHER
-        return "#{name} generated more than 50% of all commits during the past 12 months."
-      end
+  def find_count(name_to_find)
+    series.map do |data|
+      data.find { |name, _| name == name_to_find }.last
     end
-    nil
+  end
+
+  def committer_names
+    @comitter_names ||= series.map { |data| data.map(&:first) }.flatten.uniq
+  end
+
+  def warning_messages
+    committers_with_most_commits_last_year.map do |name, count|
+      "#{name} generated more than 50% of all commits during the past 12 months."
+    end
+  end
+
+  def committers_with_most_commits_last_year
+    half_of_commits_count = series[1].map(&:last).sum / 2
+    series[1].take(NAME_COUNT).reject { |datum| datum.last < half_of_commits_count }
   end
 end
