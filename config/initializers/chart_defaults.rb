3ab0ac6f53d65b6f5f838d7d443bf026545576c9
Harish Natarajan <harish.natarajan@imaginea.com>
2015-04-21 13:26:49 +0530

    OTWO-3446 Top commit volume chart finalized

--- config/initializers/chart_defaults.rb
+++ config/initializers/chart_defaults.rb
@@ -1,15 +1,16 @@
-def load_charting_defaults(source)
-  defaults = YAML.load File.read(source)
+def load_chart_options(source)
+  defaults = YAML.load File.read(Rails.root.join("config/charting/#{source}"))
   defaults.with_indifferent_access
 end
 
-CHART_DEFAULTS = load_charting_defaults("#{Rails.root}/config/charting/defaults.yml")
-COMMITS_BY_PROJECT_CHART_DEFAULTS = load_charting_defaults("#{Rails.root}/config/charting/commits_by_project.yml")
-DEMOGRAPHIC_CHART_DEFAULTS = load_charting_defaults("#{Rails.root}/config/charting/demographic.yml")
-ANALYSIS_CHART_DEFAULTS = load_charting_defaults("#{Rails.root}/config/charting/analysis_chart_defaults.yml")
-COMMIT_HISTORY_CHART_DEFAULTS = load_charting_defaults("#{Rails.root}/config/charting/analysis_commit_history_chart.yml")
-COMMITTER_HISTORY_CHART_DEFAULTS = load_charting_defaults("#{Rails.root}/config/charting/analysis_committer_history_chart.yml")
-CODE_HISTORY_CHART_DEFAULTS = load_charting_defaults("#{Rails.root}/config/charting/analysis_code_history_chart.yml")
-TOP_COMMIT_VOLUME_CHART_DEFAULTS = load_charting_defaults("#{Rails.root}/config/charting/analysis_top_commit_volume_chart.yml")
-LANGUAGE_HISTORY_CHART = load_charting_defaults("#{Rails.root}/config/charting/analysis_language_history_chart.yml")
-LINES_OF_CODE_CHART_DEFAULTS = load_charting_defaults("#{Rails.root}/config/charting/analysis_lines_of_code_chart_defaults.yml")
+CHART_DEFAULTS = load_chart_options('defaults.yml')
+COMMITS_BY_PROJECT_CHART_DEFAULTS = load_chart_options('commits_by_project.yml')
+DEMOGRAPHIC_CHART_DEFAULTS = load_chart_options('demographic.yml')
+ANALYSIS_CHART_DEFAULTS = load_chart_options('analysis/defaults.yml')
+TOP_COMMIT_VOLUME_CHART_DEFAULTS = load_chart_options('analysis/top_commit_volume.yml')
+
+COMMIT_HISTORY_CHART_DEFAULTS = load_chart_options('analysis_commit_history_chart.yml')
+COMMITTER_HISTORY_CHART_DEFAULTS = load_chart_options('analysis_committer_history_chart.yml')
+CODE_HISTORY_CHART_DEFAULTS = load_chart_options('analysis_code_history_chart.yml')
+LANGUAGE_HISTORY_CHART = load_chart_options('analysis_language_history_chart.yml')
+LINES_OF_CODE_CHART_DEFAULTS = load_chart_options('analysis_lines_of_code_chart_defaults.yml')
