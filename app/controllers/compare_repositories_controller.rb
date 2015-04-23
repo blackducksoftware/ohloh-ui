221d0f107a8ac3588c668323826fcd1de9164c7d
Alex Johnson <notalexjohnson@gmail.com>
2015-04-23 19:51:50 +0530

    OTWO-3457: Remove blank controller method definition

--- app/controllers/compare_repositories_controller.rb
+++ app/controllers/compare_repositories_controller.rb
@@ -1,9 +1,6 @@
 class CompareRepositoriesController < ApplicationController
   before_action :tool_context, only: :index
 
-  def index
-  end
-
   def chart
     render json: RepositoryComparisionChart.build.to_json, layout: false
   end
