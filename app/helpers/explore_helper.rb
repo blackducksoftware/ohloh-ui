881afc774bf28bc8e6e5ca10fa38c9dd0bf3b00c
Harish Natarajan <harish.natarajan@imaginea.com>
2015-04-28 16:33:04 +0530

    OTWO-3460 Fixed haml-lint issues

--- app/helpers/explore_helper.rb
+++ app/helpers/explore_helper.rb
@@ -1,6 +1,15 @@
 module ExploreHelper
+  COMPARE_PROJECT_INPUT_NOS = [0, 1, 2]
+
   def scale_to(count, nearest = 100)
     i = (count / nearest.to_f).ceil
     (i == 0 ? 1 : i) * nearest
+  end
+
+  def compare_project_inputs
+    COMPARE_PROJECT_INPUT_NOS.map do |n|
+      { type: 'text', name: "project_#{n}", placeholder: t('.enter_project'), rel: 'ghost',
+        autocomplete: 'off', id: "project_#{n}" }
+    end
   end
 end
