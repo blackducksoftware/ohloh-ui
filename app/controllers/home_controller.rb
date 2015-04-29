950688187bf05c6182ec95a71a7795b84cd7146b
bobofraggins <peter.fry@mac.com>
2015-04-29 14:29:12 -0400

    Generate app status variables at app launch instead of on each request.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,17 +5,6 @@ class HomeController < ApplicationController
 
   def server_info
     render json: { status: 'OK', ruby: RUBY_VERSION, rails: Rails.version,
-                   git_sha: HomeController.git_sha, passenger: HomeController.passenger_version }
-  end
-
-  class << self
-    def git_sha
-      file = "#{Rails.root}/config/GIT_SHA"
-      File.exist?(file) ? File.read(file)[0...40] : 'development'
-    end
-
-    def passenger_version
-      /([0-9\.]+)/.match(`passenger -v`)[0] rescue 'development'
-    end
+                   git_sha: Rails.application.config.git_sha, passenger: Rails.application.config.passenger_version }
   end
 end
