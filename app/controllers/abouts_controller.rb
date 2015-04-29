8094704cd94769af4ebcea4233c232f01a1fed04
Daniel Rubio <drubio1989@gmail.com>
2015-04-29 16:45:15 -0400

    Correct minor errors

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -1,7 +1,7 @@
 class AboutsController < ApplicationController
   def tools
     @languages = Language.all.order('nice_name')
-    @languages_total_sum = @languages.inject { |a, e| a + e.total }
+    @languages_total_sum = @languages.inject(0) { |a, e| a + e.total }
     render 'abouts/tools'
   end
 end
