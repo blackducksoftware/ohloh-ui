1d1a4f7549533b95a5d90e3b70e5a54b98927979
bobofraggins <peter.fry@mac.com>
2015-05-07 15:48:08 -0400

    Implement /people/rankings action.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,10 +1,13 @@
 class PeopleController < UnclaimedController
-  before_action :find_people, only: [:index]
+  helper KudosHelper
+
+  before_action :find_index_people, only: [:index]
   before_action :preload_data, only: [:index]
+  before_action :find_rankings_people, only: [:rankings]
 
   private
 
-  def find_people
+  def find_index_people
     @claimed_people = Person.find_claimed(params[:q], 'relevance').paginate(page: 1, per_page: 3)
     @unclaimed_people = Person.find_unclaimed(q: params[:q], find_by: 'relevance', per_page: 3)
     @unclaimed_people_count = Person::Count.unclaimed_by(params[:q], 'relevance')
@@ -15,5 +18,10 @@ class PeopleController < UnclaimedController
     @cbp_map = PeopleDecorator.new(@claimed_people).commits_by_project_map
     @positions_map = Position.where(id: @cbp_map.values.map(&:first).flatten).includes(:project)
                      .references(:all).index_by(&:id)
+  end
+
+  def find_rankings_people
+    @people = Person.tsearch(params[:query], params[:sort_by] || 'sort_by_kudo_position')
+              .paginate(page: params[:page] || 1, per_page: 10)
   end
 end
--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -4,21 +4,31 @@ describe 'PeopleControllerTest' do
   before do
     Account.destroy_all
     Person.destroy_all
-    account = create(:account, id: rand(9999) + 9999) # get well past the fixture data
+    account = create(:account, name: 'Bubba Hotep')
     best_vita = create(:best_vita, account: account)
     account.update_attributes(best_vita_id: best_vita.id)
-    @claimed = create(:person)
+    @claimed = create(:person, kudo_score: 12, kudo_rank: 3)
     @claimed.update_attributes(id: account.id, account_id: account.id)
-    @unclaimed = create(:person)
+    @unclaimed = create(:person, kudo_score: 12, kudo_rank: 3, effective_name: 'Bubba Amon')
     @project1 = create(:project)
     @project2 = create(:project)
-    create(:name_fact, analysis_id: @project1.best_analysis_id, name_id: @claimed.name.id)
-    create(:name_fact, analysis_id: @project2.best_analysis_id, name_id: @claimed.name.id)
+    create(:name_fact, analysis_id: @project1.best_analysis_id, name_id: @unclaimed.name.id)
+    create(:name_fact, analysis_id: @project2.best_analysis_id, name_id: @unclaimed.name.id)
+    @unclaimed.name.update_attributes(name: 'Bubba Amon')
   end
 
   describe 'index' do
     it 'should render the people found' do
-      get :index
+      get :index, q: 'Bubba'
+      must_respond_with :ok
+      response.body.must_match @claimed.account.name
+      response.body.must_match @unclaimed.name.name
+    end
+  end
+
+  describe 'rankings' do
+    it 'should render the people found' do
+      get :rankings, query: 'Bubba'
       must_respond_with :ok
       response.body.must_match @claimed.account.name
       response.body.must_match @unclaimed.name.name
