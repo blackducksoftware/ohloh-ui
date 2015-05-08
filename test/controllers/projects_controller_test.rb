1352c79fd801b79fe742a668a76d1905f2c37bb3
Harish Natarajan <harish.natarajan@imaginea.com>
2015-05-08 11:14:44 +0530

    OTWO-3482 Fixed failing test in projects controller

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -552,35 +552,35 @@ class ProjectsControllerTest < ActionController::TestCase
     response.body.wont_match 'Arizona'
   end
 
-  describe 'similar' do
-    it 'should return similar projects (both by tags and stacks)' do
-      project1 = create(:project)
-      project2 = create(:project)
-      project3 = create(:project)
-      stack1 = create(:stack)
-      stack2 = create(:stack)
-      stack3 = create(:stack)
+  it '#similar should return similar projects (both by tags and stacks)' do
+    project1 = create(:project)
+    project2 = create(:project)
+    project3 = create(:project)
+    stack1 = create(:stack)
+    stack2 = create(:stack)
+    stack3 = create(:stack)
 
-      create(:stack_entry, stack: stack1, project: project1)
-      create(:stack_entry, stack: stack1, project: project2)
-      create(:stack_entry, stack: stack1, project: project3)
-      create(:stack_entry, stack: stack2, project: project1)
-      create(:stack_entry, stack: stack2, project: project2)
-      create(:stack_entry, stack: stack2, project: project3)
-      create(:stack_entry, stack: stack3, project: project1)
-      create(:stack_entry, stack: stack3, project: project2)
-      create(:stack_entry, stack: stack3, project: project3)
+    create(:stack_entry, stack: stack1, project: project1)
+    create(:stack_entry, stack: stack1, project: project2)
+    create(:stack_entry, stack: stack1, project: project3)
+    create(:stack_entry, stack: stack2, project: project1)
+    create(:stack_entry, stack: stack2, project: project2)
+    create(:stack_entry, stack: stack2, project: project3)
+    create(:stack_entry, stack: stack3, project: project1)
+    create(:stack_entry, stack: stack3, project: project2)
+    create(:stack_entry, stack: stack3, project: project3)
 
-      tag = create(:tag)
-      create(:tagging, tag: tag, taggable: project1)
-      create(:tagging, tag: tag, taggable: project2)
-      create(:tagging, tag: tag, taggable: project3)
+    tag = create(:tag)
+    create(:tagging, tag: tag, taggable: project1)
+    create(:tagging, tag: tag, taggable: project2)
+    create(:tagging, tag: tag, taggable: project3)
 
-      get :similar, id: project1.to_param
+    get :similar, id: project1.to_param
 
-      assigns(:project).must_equal project1
-      assigns(:similar_by_tags).must_equal [project2, project3]
-      assigns(:similar_by_stacks).must_equal [project2, project3]
-    end
+    assigns(:project).must_equal project1
+    assigns(:similar_by_tags).must_include project2
+    assigns(:similar_by_tags).must_include project3
+    assigns(:similar_by_stacks).must_include project2
+    assigns(:similar_by_stacks).must_include project3
   end
 end
