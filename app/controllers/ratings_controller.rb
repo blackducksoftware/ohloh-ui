f78481dd47da066b205be0fe9724726cdec7a7da
Paddy <rapbhan@gmail.com>
2015-05-15 10:11:47 +0530

    Fixed the Brakeman Security Warnings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,16 +1,18 @@
 class RatingsController < ApplicationController
+  ALLOWED_PARTIALS = ['projects/show/community_rating', 'reviews/rater']
   before_action :session_required
   before_action :find_project_and_rating
+  before_action :sanitize_partial, only: [:rate, :unrate]
 
   def rate
     @rating.assign_attributes(model_params)
     @rating.save
-    render partial: params[:show], locals: { score: @rating.score, project: @project.reload }
+    render partial: @partial, locals: { score: @rating.score, project: @project.reload }
   end
 
   def unrate
     @rating.destroy if @rating.persisted?
-    render partial: params[:show], locals: { score: '0' }
+    render partial: @partial, locals: { score: '0' }
   end
 
   private
@@ -24,5 +26,13 @@ class RatingsController < ApplicationController
     @rating = Rating.where(project_id: @project.id, account_id: current_user.id).first_or_initialize
   rescue ActiveRecord::RecordNotFound
     raise ParamRecordNotFound
+  end
+
+  def sanitize_partial
+    if ALLOWED_PARTIALS.include? params[:show]
+      @partial = params[:show]
+    else
+      fail StandardError, I18n.t('ratings.partial_not_found', partial: params[:show])
+    end
   end
 end
