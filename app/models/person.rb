61fb7c5992789620968d9e8500d9624f3eea8be5
santhanakarthikeyan <santhanakarthikeyan.s@imaginea.com>
2015-05-21 12:58:43 +0530

    Fix encoding issue in accounts related pages

--- app/models/person.rb
+++ app/models/person.rb
@@ -43,6 +43,7 @@ class Person < ActiveRecord::Base
       sort_by = sort_by.eql?('kudo_position') ? 'sort_by_kudo_position' : nil
       tsearch(query, sort_by)
         .includes(:account)
+        .preload(account: [:projects, :markup])
         .where.not(account_id: nil)
         .references(:all)
     end
