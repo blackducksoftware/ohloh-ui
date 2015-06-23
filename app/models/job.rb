9adf7ea5e0cf970d18be50cd8542b3109bb33eab
Paddy <rapbhan@gmail.com>
2015-06-23 16:12:04 +0530

    OTWO-3665 Moved the OrgJob to a separate file

--- app/models/job.rb
+++ app/models/job.rb
@@ -14,7 +14,3 @@ class Job < ActiveRecord::Base
     end
   end
 end
-
-class OrganizationJob < Job
-  # Doesn't need any methods as it would be processed by the Crawlers running with old ohloh sourse.
-end
