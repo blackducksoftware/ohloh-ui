b37bf67d59342dae75d497d5461685550c86abf4
Harish Natarajan <harish.natarajan@imaginea.com>
2015-06-24 13:27:02 +0530

    OTWO-3628 Added admin duplicates show page

--- test/integration/admin/duplicates_admin_test.rb
+++ test/integration/admin/duplicates_admin_test.rb
@@ -9,4 +9,13 @@ class DuplicatesAdminTest < ActionDispatch::IntegrationTest
     get admin_duplicates_path
     assert_response :success
   end
+
+  it 'show loads' do
+    duplicate = create(:duplicate)
+    admin = create(:admin, password: 'xyzzy123456')
+    admin.password = 'xyzzy123456'
+    login_as admin
+    get admin_duplicate_path(duplicate)
+    assert_response :success
+  end
 end
