4fd69a501c908ef4c27145e45869e70502798d0e
Alex Johnson <notalexjohnson@gmail.com>
2015-06-29 12:13:49 +0530

    OTWO-3661 Verify repository urls using OhlohScm

--- test/models/analysis_sloc_set_test.rb
+++ test/models/analysis_sloc_set_test.rb
@@ -16,6 +16,13 @@ class AnalysisSlocSetTest < ActiveSupport::TestCase
   end
 
   describe 'ignore_tuples' do
+    let(:source_scm_class) { SvnSyncRepository.new.source_scm_class }
+
+    before do
+      source_scm_class.any_instance.stubs(:validate_server_connection)
+      source_scm_class.any_instance.stubs(:restrict_url_to_trunk).returns(Faker::Internet.url)
+    end
+
     it 'should return parsed file names' do
       create_analysis_sloc_set('Disallow: foo.txt')
       code_set = " and fyles.code_set_id = #{@analysis_sloc_set.sloc_set.code_set_id}"
@@ -51,7 +58,7 @@ class AnalysisSlocSetTest < ActiveSupport::TestCase
   end
 
   def create_svn_sync_repository(ignore = '')
-    repository = create(:svnsyncrepository)
+    repository = create(:svn_sync_repository)
     code_set = create(:code_set, repository: repository)
     sloc_set = create(:sloc_set, code_set: code_set)
     @analysis_sloc_set = create(:analysis_sloc_set, sloc_set: sloc_set, ignore: ignore)
---
+++ test/models/hg_repository_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class HgRepositoryTest < ActiveSupport::TestCase
+  let(:source_scm_class) { HgRepository.new.source_scm_class }
+
+  before { source_scm_class.any_instance.stubs(:validate_server_connection) }
+
+  it 'must find existing repository by url' do
+    repository = create(:hg_repository, bypass_url_validation: false)
+
+    HgRepository.find_existing(repository).must_equal repository
+  end
+end
