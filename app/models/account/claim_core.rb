f01fe71effb50c95ef1ac8a555b00d0ae7fb2f45
Harish Natarajan <harish.natarajan@imaginea.com>
2015-07-06 16:39:00 +0530

    OTWO-3653 Removed fixme's no longer needed in acoounts controller and account claim core

--- app/models/account/claim_core.rb
+++ app/models/account/claim_core.rb
@@ -1,16 +1,13 @@
 class Account::ClaimCore < OhDelegator::Base
-  # FIXME: Replace claimed_email_ids with this.
   def email_ids
     email_ids_query.pluck(:email_address_ids).flatten.uniq
   end
 
-  # FIXME: Replace claimed_emails with this.
   def emails
     return [] if email_ids.empty?
     EmailAddress.where(id: email_ids).pluck(:address)
   end
 
-  # No of unclaimed persons based on account's claimed emails.
   def unclaimed_persons_count
     return 0 if emails.empty?
     Person::Count.unclaimed_by(emails.join(' '), 'email')
