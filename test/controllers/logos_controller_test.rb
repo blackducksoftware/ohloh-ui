2be3bc8bc2175bf63cf6e9dddffcd756ab566f3a
Harish Natarajan <harish.natarajan@imaginea.com>
2015-07-14 21:32:02 +0530

    OTWO-3701 Converted fixtures to factories for logos controller tests

--- test/controllers/logos_controller_test.rb
+++ test/controllers/logos_controller_test.rb
@@ -1,16 +1,17 @@
 require_relative '../test_helper.rb'
 
 class LogosControllerTest < ActionController::TestCase
+  let(:project) { create(:project) }
+
   def setup
     ActionView::Base.any_instance.stubs(:current_user_can_manage?).returns('true')
     @admin = create(:admin)
     @user = create(:account)
+    create(:account, login: 'ohloh_slave')
   end
 
-  it 'user has permissions to edit' do
-    Project.any_instance.expects(:edit_authorized?).returns(false)
-    login_as @user
-    post :create, project_id: projects(:linux).id
+  it 'user should be redirected to login page' do
+    post :create, project_id: project.id
     must_respond_with :redirect
     must_redirect_to '/sessions/new'
   end
@@ -18,8 +19,7 @@ class LogosControllerTest < ActionController::TestCase
   it 'upload logo via URL' do
     VCR.use_cassette('LogoClearGif') do
       login_as @admin
-      project = projects(:linux)
-      Project.any_instance.expects(:edit_authorized?).returns(true)
+      Project.any_instance.stubs(:edit_authorized?).returns(true)
       post :create, project_id: project.id, logo: { url: 'https://www.openhub.net/images/clear.gif' }
       must_redirect_to new_project_logos_path
       project.reload.logo.attachment_file_name.must_equal 'clear.gif'
@@ -42,7 +42,6 @@ class LogosControllerTest < ActionController::TestCase
   it 'validate failure'do
     VCR.use_cassette('LogoClearGif') do
       login_as @admin
-      project = projects(:linux)
       Project.any_instance.expects(:edit_authorized?).at_least_once.returns(true)
       post :create, project_id: project.id, logo: { url: 'https://www.dummyhost.net/images/clear.gif' }
       must_respond_with :unprocessable_entity
@@ -53,8 +52,7 @@ class LogosControllerTest < ActionController::TestCase
 
   it 'upload logo via desktop file' do
     login_as @admin
-    project = projects(:linux)
-    Project.any_instance.expects(:edit_authorized?).returns(true)
+    Project.any_instance.stubs(:edit_authorized?).returns(true)
     tempfile = Rack::Test::UploadedFile.new('test/fixtures/files/ruby.png', 'image/png')
     post :create, project_id: project.id, logo: { attachment: tempfile }
     must_redirect_to new_project_logos_path
@@ -63,8 +61,6 @@ class LogosControllerTest < ActionController::TestCase
   end
 
   it 'open LogosController new for project with no logo renders successfully' do
-    project = projects(:linux)
-
     get :new, project_id: project.id
     must_respond_with :success
   end
@@ -77,7 +73,6 @@ class LogosControllerTest < ActionController::TestCase
   end
 
   it 'LogosController destroy resets to NilLogo' do
-    project = create(:project)
     login_as @admin
 
     delete :destroy, project_id: project.id
@@ -87,14 +82,14 @@ class LogosControllerTest < ActionController::TestCase
   end
 
   it 'new unauthenticated' do
-    get :new, project_id: projects(:linux).id
+    get :new, project_id: project.id
     must_respond_with :success
   end
 
   it 'must render the new page successfully' do
     login_as @admin
 
-    get :new, project_id: projects(:linux).id
+    get :new, project_id: project.id
 
     must_respond_with :success
     must_render_template :new
@@ -113,12 +108,12 @@ class LogosControllerTest < ActionController::TestCase
 
   it 'create with logo id' do
     login_as @admin
-    Project.any_instance.expects(:edit_authorized?).returns(true)
+    Project.any_instance.stubs(:edit_authorized?).returns(true)
     desired_new_logo_id = create(:attachment).id
-    post :create, project_id: projects(:linux).id, logo_id: desired_new_logo_id
-    must_redirect_to new_project_logos_path(projects(:linux).id)
-    projects(:linux).reload
-    desired_new_logo_id.must_equal projects(:linux).logo_id
+    post :create, project_id: project.id, logo_id: desired_new_logo_id
+    must_redirect_to new_project_logos_path(project.id)
+    project.reload
+    desired_new_logo_id.must_equal project.logo_id
   end
 
   it 'create requires permissions' do
