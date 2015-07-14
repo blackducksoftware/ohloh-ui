cf07ae9113fda6ec0cda3b551488f29eefddd300
Harish Natarajan <harish.natarajan@imaginea.com>
2015-07-14 20:54:59 +0530

    OTWO-3701 Converted fixtures to factories for managers controller tests

--- test/controllers/managers_controller_test.rb
+++ test/controllers/managers_controller_test.rb
@@ -1,12 +1,18 @@
 require 'test_helper'
 
 class ManagersControllerTest < ActionController::TestCase
+  let(:admin) { create(:admin) }
+  let(:account) { create(:account) }
+  let(:joe_account) { create(:account, name: 'joe') }
+  let(:account_with_umlaut) { create(:account, name: 'ütvon îåa') }
+
   before do
     @proj = create(:project)
+    create(:account, login: 'ohloh_slave')
   end
 
   it 'test index of project for manager' do
-    login_as accounts(:admin)
+    login_as admin
     setup_admin_user_umlaut
     get :index, project_id: @proj.to_param
     must_respond_with :success
@@ -14,7 +20,7 @@ class ManagersControllerTest < ActionController::TestCase
   end
 
   it 'test index for i manage this project button for admin' do
-    login_as accounts(:admin)
+    login_as admin
     get :index, project_id: @proj.to_param
     must_respond_with :success
     must_select 'a.btn.btn-primary'
@@ -22,10 +28,10 @@ class ManagersControllerTest < ActionController::TestCase
   end
 
   it 'test index for i manage this project button for admin when admin is manager' do
-    login_as accounts(:admin)
+    login_as admin
     a = @proj
-    a.editor_account = accounts(:admin)
-    a.managers << accounts(:admin)
+    a.editor_account = admin
+    a.managers << admin
     a.save
     get :index, project_id: @proj.to_param
     must_respond_with :success
@@ -33,129 +39,129 @@ class ManagersControllerTest < ActionController::TestCase
   end
 
   it 'test index for applicant' do
-    login_as accounts(:umlaut)
+    login_as account_with_umlaut
     setup_admin_user_umlaut
     get :index, project_id: @proj.to_param
     must_respond_with :success
     @response.body.must_match(/Pending/i)
-    @response.body.must_match(/#{accounts(:umlaut).name}/i)
+    @response.body.must_match(/#{account_with_umlaut.name}/i)
   end
 
   it 'test index for random' do
-    login_as accounts(:joe)
+    login_as joe_account
     setup_admin_user_umlaut
     get :index, project_id: @proj.to_param
     must_respond_with :success
     @response.body.wont_match(/Pending/i)
-    @response.body.wont_match(/#{accounts(:umlaut).name}/i)
+    @response.body.wont_match(/#{account_with_umlaut.name}/i)
   end
 
   it 'test index of non existant project' do
-    login_as accounts(:admin)
+    login_as admin
     get :index, project_id: 'I_AM_A_BANANA!'
     must_respond_with :not_found
   end
 
   it 'test index of non existant organization' do
-    login_as accounts(:admin)
+    login_as admin
     get :index, organization_id: 'I_AM_A_BANANA!'
     must_respond_with :not_found
   end
 
   it 'test reject should fail unless logged in' do
-    manager_apply(accounts(:user), @proj)
-    set_manager(accounts(:admin), @proj)
-    login_as accounts(:admin)
+    manager_apply(account, @proj)
+    set_manager(admin, @proj)
+    login_as admin
     assert_difference 'Manage.where.not(deleted_by: nil).count' do
-      post :reject, project_id: @proj.to_param, id: accounts(:user).to_param
+      post :reject, project_id: @proj.to_param, id: account.to_param
     end
     must_respond_with :redirect
   end
 
   it 'test reject should succeed for self' do
-    manager_apply(accounts(:user), @proj)
-    login_as accounts(:user)
+    manager_apply(account, @proj)
+    login_as account
     assert_difference 'Manage.where.not(deleted_by: nil).count' do
-      post :reject, project_id: @proj.to_param, id: accounts(:user).to_param
+      post :reject, project_id: @proj.to_param, id: account.to_param
     end
     must_respond_with :redirect
   end
 
   it 'test reject should succeed for orgs' do
     org = create(:organization)
-    manager_apply(accounts(:user), org)
-    login_as accounts(:user)
+    manager_apply(account, org)
+    login_as account
     assert_difference 'Manage.where.not(deleted_by: nil).count' do
-      post :reject, organization_id: org.to_param, id: accounts(:user).to_param
+      post :reject, organization_id: org.to_param, id: account.to_param
     end
     must_respond_with :redirect
   end
 
   it 'test reject should not succeed for unlogged in' do
-    manager_apply(accounts(:user), @proj)
+    manager_apply(account, @proj)
     assert_no_difference 'Manage.where.not(deleted_by: nil).count' do
-      post :reject, project_id: @proj.to_param, id: accounts(:user).to_param
+      post :reject, project_id: @proj.to_param, id: account.to_param
     end
     must_respond_with :redirect
     must_redirect_to new_session_path
   end
 
   it 'test accept should succeed for manager' do
-    login_as accounts(:admin)
-    set_manager(accounts(:admin), @proj)
-    manager_apply(accounts(:user), @proj)
+    login_as admin
+    set_manager(admin, @proj)
+    manager_apply(account, @proj)
     assert_difference 'Manage.where.not(approved_by: nil).where(deleted_at: nil).count' do
-      post :approve, project_id: @proj.to_param, id: accounts(:user).to_param
+      post :approve, project_id: @proj.to_param, id: account.to_param
     end
-    @proj.active_managers.include?(accounts(:user)).must_equal true
-    manage = Manage.where(target: @proj, account_id: accounts(:user).id).first
-    manage.approver.must_equal accounts(:admin)
+    @proj.active_managers.include?(account).must_equal true
+    manage = Manage.where(target: @proj, account_id: account.id).first
+    manage.approver.must_equal admin
     must_respond_with :redirect
   end
 
   it 'test accept should fail for not logged in' do
-    set_manager(accounts(:admin), @proj)
-    manager_apply(accounts(:user), @proj)
+    set_manager(admin, @proj)
+    manager_apply(account, @proj)
     assert_no_difference 'Manage.where.not(approved_by: nil).where(deleted_at: nil).count' do
-      post :approve, project_id: @proj.to_param, id: accounts(:user).to_param
+      post :approve, project_id: @proj.to_param, id: account.to_param
     end
-    @proj.active_managers.include?(accounts(:user)).must_equal false
+    @proj.active_managers.include?(account).must_equal false
     must_respond_with :redirect
     must_redirect_to new_session_path
   end
 
   it 'test accept should fail for non manager' do
-    login_as accounts(:joe)
-    set_manager(accounts(:admin), @proj) # joe's logged in but admin's the manager
-    manager_apply(accounts(:user), @proj)
+    login_as joe_account
+    set_manager(admin, @proj) # joe's logged in but admin's the manager
+    manager_apply(account, @proj)
     assert_no_difference 'Manage.where.not(approved_by: nil).where(deleted_at: nil).count' do
-      post :approve, project_id: @proj.to_param, id: accounts(:user).to_param
+      post :approve, project_id: @proj.to_param, id: account.to_param
     end
-    @proj.active_managers.include?(accounts(:user)).must_equal false
+    @proj.active_managers.include?(account).must_equal false
     must_respond_with :unauthorized
   end
 
   it 'test accept should fail for rejected manager' do
-    login_as accounts(:joe)
-    set_manager(accounts(:joe), @proj)
-    @proj.manages.each { |pa| pa.update_attributes!(deleted_by: 1, deleted_at: Time.current) }
-    set_manager(accounts(:admin), @proj) # auto-approved
-    manager_apply(accounts(:user), @proj)
+    login_as joe_account
+    set_manager(joe_account, @proj)
+    @proj.manages.each { |pa| pa.update_attributes!(deleted_by: admin.id, deleted_at: Time.current) }
+    set_manager(admin, @proj) # auto-approved
+    manager_apply(account, @proj)
     assert_no_difference 'Manage.where.not(approved_by: nil).where(deleted_at: nil).count' do
-      post :approve, project_id: @proj.to_param, id: accounts(:user).to_param
+      post :approve, project_id: @proj.to_param, id: account.to_param
     end
-    @proj.active_managers.include?(accounts(:user)).must_equal false
+    @proj.active_managers.include?(account).must_equal false
     must_respond_with :unauthorized
   end
 
   it 'test new' do
-    login_as accounts(:admin)
+    login_as admin
     get :new, project_id: @proj.to_param
     must_respond_with :success
   end
 
   it 'test create' do
-    login_as accounts(:admin)
+    login_as admin
     assert_difference 'Manage.count' do
       post :create, project_id: @proj.to_param, manage: { message: 'testing' }
       must_respond_with :redirect
@@ -163,7 +169,7 @@ class ManagersControllerTest < ActionController::TestCase
   end
 
   it 'test create doesnt allow books' do
-    login_as accounts(:admin)
+    login_as admin
     assert_no_difference 'Manage.count' do
       post :create, project_id: @proj.to_param, manage: { message: 'testing' * 1_000 }
       must_respond_with :unprocessable_entity
@@ -171,9 +177,9 @@ class ManagersControllerTest < ActionController::TestCase
   end
 
   it 'test edit' do
-    login_as accounts(:admin)
+    login_as admin
     admin_manages_linux
-    get :edit, project_id: @proj.to_param, id: accounts(:admin).to_param
+    get :edit, project_id: @proj.to_param, id: admin.to_param
     must_respond_with :success
     assigns(:manage).message.must_equal 'test message'
   end
@@ -199,32 +205,32 @@ class ManagersControllerTest < ActionController::TestCase
   end
 
   it 'test edit of a non existant account' do
-    login_as accounts(:admin)
+    login_as admin
     get :edit, project_id: @proj.to_param, id: 'I_AM_A_BANANA!'
     must_respond_with :not_found
   end
 
   it 'test update' do
-    login_as accounts(:admin)
+    login_as admin
     admin_manages_linux
-    post :update, id: accounts(:admin).to_param, project_id: @proj.to_param, manage: { message: 'hihi' }
+    post :update, id: admin.to_param, project_id: @proj.to_param, manage: { message: 'hihi' }
     @proj.reload.manages.first.message.must_equal 'hihi'
     assert flash[:notice] == 'Save successful!'
   end
 
   it 'test update doesnt allow books' do
-    login_as accounts(:admin)
+    login_as admin
     admin_manages_linux
-    post :update, id: accounts(:admin).to_param, project_id: @proj.to_param,
+    post :update, id: admin.to_param, project_id: @proj.to_param,
                   manage: { message: 'testing' * 1_000 }
     must_respond_with :unprocessable_entity
     @proj.reload.manages.first.message.must_equal 'test message'
   end
 
   it 'test update wont edit someone elses' do
-    login_as accounts(:user)
+    login_as account
     admin_manages_linux
-    post :update, id: accounts(:admin).to_param, project_id: @proj.to_param, manage: { message: 'hihi' }
+    post :update, id: admin.to_param, project_id: @proj.to_param, manage: { message: 'hihi' }
     must_respond_with :unauthorized
     @proj.manages.first.message.wont_equal 'hihi'
   end
@@ -233,7 +239,7 @@ class ManagersControllerTest < ActionController::TestCase
 
   def set_manager(account, project)
     manage = project.manages.create!(account: account, message: 'let me in!')
-    manage.update_attributes(approved_by: 1)
+    manage.update_attributes(approved_by: admin.id)
     manage
   end
 
@@ -244,13 +250,13 @@ class ManagersControllerTest < ActionController::TestCase
   end
 
   def setup_admin_user_umlaut
-    set_manager(accounts(:admin), @proj)
-    set_manager(accounts(:user), @proj)
-    manager_apply(accounts(:umlaut), @proj)
+    set_manager(admin, @proj)
+    set_manager(account, @proj)
+    manager_apply(account_with_umlaut, @proj)
   end
 
   def admin_manages_linux
     @proj.manages.delete_all
-    @proj.manages.create!(account_id: accounts(:admin).id, approved_by: accounts(:user).id, message: 'test message')
+    @proj.manages.create!(account_id: admin.id, approved_by: account.id, message: 'test message')
   end
 end
