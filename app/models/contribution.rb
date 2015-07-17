54fd860f6e952107c9ec9a8dc78b588dac4cab96
bobofraggins <peter.fry@mac.com>
2015-07-17 10:54:29 -0400

    Don't throw uncaught exception if user asked for sort we don't support. Just give them the default sort.

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -53,8 +53,7 @@ class Contribution < ActiveRecord::Base
 
   class << self
     def sort(key)
-      key ||= :twelve_month_commits
-      fail ArgumentError, 'invalid sort option' unless SORT_OPTIONS.include?(key.to_sym)
+      key = :twelve_month_commits unless key && SORT_OPTIONS.include?(key.to_sym)
       send("sort_by_#{key}")
     end
 
