3b4c837ecbbda856d49ee8643c8d208479302a09
Paddy <rapbhan@gmail.com>
2015-07-23 11:24:56 +0530

    OTWO-3782 Find Account login by case insesitive

--- app/models/account/find.rb
+++ app/models/account/find.rb
@@ -1,7 +1,7 @@
 class Account::Find
   class << self
     def by_id_or_login(value)
-      Account.where('id = :id or login = :login', id: value.to_i, login: value).first
+      Account.where('id = :id or lower(login) = :login', id: value.to_i, login: value.downcase).first
     end
   end
 end
