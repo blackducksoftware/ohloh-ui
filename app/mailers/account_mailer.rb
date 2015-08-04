35f86e30a1f8700279e4fa461622127d9ab4e5fa
Daniel Rubio <drubio1989@gmail.com>
2015-08-04 16:50:36 -0400

    Correcting Activation link --WIP

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -9,7 +9,8 @@ class AccountMailer < ActionMailer::Base
   end
 
   def activation(account)
-    @url = root_url(host: ENV['URL_HOST'])
+    @url = activate_account_accesses_url(account_id: account.to_param, code: account.activation_code,
+                                         host: ENV['URL_HOST'], protocol: 'https')
     @account = account
     mail to: account.email, subject: t('.subject')
   end
--- test/mailers/account_mailer_test.rb
+++ test/mailers/account_mailer_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-describe AccountMailer do
+class AccountMailerTest < ActionMailer::TestCase
   it '#signup_notification' do
     user = create(:account, activated_at: nil)
     before = ActionMailer::Base.deliveries.count
@@ -10,5 +10,28 @@ describe AccountMailer do
     email[:from].value.must_equal 'mailer@openhub.net'
     email.subject.must_equal I18n.t('account_mailer.signup_notification.subject')
     email.body.encoded.must_match I18n.t('.account_mailer.signup_notification.body', login: user.login)
+  end
+
+  it '#activation' do
+
+    # TODO: Ask PDP about where the activation email comes from. 
+    # Does it come from the old architecture because its created in prd-oh-utility?
+    # Or does the mail come from ohloh-ui
+    # In short does it come from ohloh or ohloh-ui
+
+    # Ask if any part of the production architecture still comes from ohloh.
+
+    user = create(:account, activated_at: nil)
+    user.update!(activated_at: Time.now, activation_code: Faker::Lorem.characters(20))
+    activation_email = ActionMailer::Base.deliver_now
+    # user should receive an email
+    #check all the fields
+
+  end
+
+  it '#reset_password_link' do
+  end
+
+  it '#kudo_recipient' do
   end
 end
