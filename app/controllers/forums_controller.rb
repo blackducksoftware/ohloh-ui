ddec7d80702e0222ee65208b657a978f5eefbf50
Daniel Rubio <drubio1989@gmail.com>
2015-08-04 10:35:14 -0400

    OTWO-3804 remove before_action new and create

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -2,7 +2,7 @@ class ForumsController < ApplicationController
   before_action :find_most_recent_topics_from_forum, only: :index
   before_action :find_forum_record, except: [:index, :new, :create]
   before_action :session_required, except: [:index, :show]
-  before_action :admin_session_required, except: [:index, :show, :new, :create]
+  before_action :admin_session_required, except: [:index, :show]
 
   def index
     # General Discussion is not on the production website.
