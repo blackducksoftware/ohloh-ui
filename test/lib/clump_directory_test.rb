e1812ccfb228f9362f5d677d0fc63174cf3b3409
Alex Johnson <notalexjohnson@gmail.com>
2015-08-11 12:16:03 +0530

    OTWO-3729 Fix failing clump directory test
    Test failed on drives without the parent /var/spool/clumps/000/
    directory.

--- test/lib/clump_directory_test.rb
+++ test/lib/clump_directory_test.rb
@@ -11,6 +11,8 @@ class ClumpDirectoryTest < ActiveSupport::TestCase
     it 'must return all code_set_ids using path to physical clumps on disk' do
       paths_on_disk = '/var/spool/clumps/000/000/000/158 /var/spool/clumps/000/000/000/162'
       Slave.any_instance.stubs(:run_on_clump_machine).returns(paths_on_disk)
+      File.stubs(:exist?).returns(true)
+
       ClumpDirectory.code_set_ids.must_equal([158, 162])
     end
   end
