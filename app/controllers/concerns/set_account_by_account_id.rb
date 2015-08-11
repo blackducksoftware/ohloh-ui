0a12611cbf5450c40049141e3fcbd1364f423ed4
Paddy <rapbhan@gmail.com>
2015-08-11 11:02:59 +0530

    Fixed few UI issues

--- app/controllers/concerns/set_account_by_account_id.rb
+++ app/controllers/concerns/set_account_by_account_id.rb
@@ -10,9 +10,7 @@ module SetAccountByAccountId
   private
 
   def set_account_by_account_id
-    @account = Rails.cache.fetch("set_account_id_#{params[:account_id]}", expires_in: 24.hours) do
-      Account::Find.by_id_or_login(params[:account_id])
-    end
+    @account = Account::Find.by_id_or_login(params[:account_id])
     fail ParamRecordNotFound unless @account
   end
 end
