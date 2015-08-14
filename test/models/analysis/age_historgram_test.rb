c85ee1b72090c371b99a471d1689fc41c0979141
Harish Natarajan <harish.natarajan@imaginea.com>
2015-08-14 16:35:45 +0530

    OTWO-3865 Fixed rubocop warnings in analyis spark related methods's tests

--- test/models/analysis/age_historgram_test.rb
+++ test/models/analysis/age_historgram_test.rb
@@ -7,9 +7,9 @@ class AgeHistorgramTest < ActiveSupport::TestCase
 
       histogram_data = Analysis::AgeHistogram.execute
       histogram_data.length.must_equal 3
-      histogram_data.first.logged_date.must_equal (Date.today - 2.days).to_time(:utc)
+      histogram_data.first.logged_date.must_equal((Date.today - 2.days).to_time(:utc))
       histogram_data.first.value.must_equal 2
-      histogram_data.second.logged_date.must_equal (Date.today - 1.day).to_time(:utc)
+      histogram_data.second.logged_date.must_equal((Date.today - 1.day).to_time(:utc))
       histogram_data.second.value.must_equal 2
       histogram_data.third.logged_date.must_equal Date.today.to_time(:utc)
       histogram_data.third.value.must_equal 2
