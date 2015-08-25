00fef5133289fa4119973e1a0bd7416a5b6823cd
Peter Degen-Portnoy <pdegenportnoy@blackducksoftware.com>
2015-08-25 13:01:32 -0400

    Fix the broken hooks_test test
    The core issue was that the Ohloh Hamster account needs to be an
    administrator.  Simply setting the level in the "create_" method wasn't
    sufficient because *somewhere* there is a record inserted into the
    accounts table with an ID 1 (this is used by other factories).
    
    Really, I wasn't able to track it down.  I'm sure it's hidden in plain
    sight.  As a solution, I just make sure that the found or created record
    is set to the administrative level.  One notes that this account in
    production is indeed an administrator.  And therefore, should not allow
    anyone to log into it.

--- test/test_helpers/setup_hamster_account.rb
+++ test/test_helpers/setup_hamster_account.rb
@@ -1,7 +1,8 @@
 module SetupHamsterAccount
   def create_hamster_account
     account = Account.where(login: 'ohloh_slave').first
-    account || create_new_account
+    account ||= create_new_account
+    account.update_attribute(:level, 10) if account
   end
 
   private
