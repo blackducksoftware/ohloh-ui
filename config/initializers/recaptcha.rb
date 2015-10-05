121ab01b6e54464092d7ed8e58897359052e7231
Alex Johnson <notalexjohnson@gmail.com>
2015-10-05 14:56:14 +0530

    OTWO-3976 Replace hardcoded recaptcha key

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
-  config.public_key = '6Lc4IgsTAAAAAD_aMrJhr8o9zg8rl5yZcmZB5OiW'
+  config.public_key = ENV['RECAPTCHA_PUBLIC_KEY']
   config.private_key = ENV['RECAPTCHA_SECRET_KEY']
   config.api_version = 'v2'
 end
