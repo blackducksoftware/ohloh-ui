c51007eab3de01e739904af1eb0a9f0a0178091c
Paddy <rapbhan@gmail.com>
2015-11-04 12:52:04 +0530

    OTWO-3826 Moved API Keys to ActiveAdmin

--- app/admin/api_keys_admin.rb
+++ app/admin/api_keys_admin.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register ApiKey do
-  filter :key
+  filter :oauth_application_secret_or_key, as: :string, label: 'Key'
   filter :name
   filter :description
   filter :total_count
@@ -15,12 +15,16 @@ ActiveAdmin.register ApiKey do
   index do
     column :id
     column :name do |api_key|
-      link_to api_key.name, admin_api_key_path(api_key)
+      name = api_key.name || api_key.try(:oauth_application).try(:name)
+      link_to name, admin_api_key_path(api_key)
     end
     column :account do |api_key|
       link_to api_key.account.name, admin_account_path(api_key.account)
     end
-    column :key
+    column :key do |api_key|
+      key = api_key.try(:oauth_application).try(:secret) || api_key.key || I18n.t('api_keys.none')
+      truncate(key, omision: '...', length: 20)
+    end
     column :status do |api_key|
       case api_key.status
       when ApiKey::STATUS_OK
@@ -34,6 +38,31 @@ ActiveAdmin.register ApiKey do
     column :total_count
     column :daily_count
     actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :created_at
+      row :account
+      row :key
+      row :description
+      row :daily_count
+      row :daily_limit
+      row :day_began_at
+      row :last_access_at
+      row :total_count
+      row :total_count
+      row :name
+      row :url
+      row :support_url
+      row :callback_url
+      row :secret
+      row :oauth_application_id
+      row :oauth_secret_key do |api_key|
+        key = api_key.try(:oauth_application).try(:secret)
+      end
+    end
   end
 
   form do |f|
