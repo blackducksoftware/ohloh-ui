b813be5b435693e40839145ade12a0f264fcaa4b
Harish Natarajan <harish.natarajan@imaginea.com>
2015-11-16 18:56:39 +0530

    OTWO-4033 Added tests for complete , sloc , fetch admin pages

---
+++ test/factories/jobs.rb
@@ -0,0 +1,18 @@
+FactoryGirl.define do
+  factory :job do
+    slave Slave.where(hostname: Socket.gethostname).first_or_create!
+    status Job::STATUS_SCHEDULED
+  end
+
+  factory :fetch_job, parent: :job, class: :FetchJob do
+    type 'FetchJob'
+  end
+
+  factory :sloc_job, parent: :job, class: :SlocJob do
+    type 'SlocJob'
+  end
+
+  factory :complete_job, parent: :job, class: :CompleteJob do
+    type 'CompleteJob'
+  end
+end
