e4351005b2442c2fa6d134df769b6f7c15e8ba2c
Paddy <rapbhan@gmail.com>
2016-02-02 12:41:36 +0530

    OTWO-4079 Fixed the CORS header

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,29 +1,10 @@
 class FeedbacksController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
-  before_action :cors_preflight_check
-  after_action :cors_set_access_control_headers
-
-  # For all responses in this controller, return the CORS access control headers.
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = set_access_control_header
-    headers['Access-Control-Allow-Methods'] = 'POST'
-    headers['Access-Control-Max-Age'] = '1728000' # in seconds ..48hours (60*60*48)
-  end
-
-  # If this is a preflight OPTIONS request, then short-circuit the
-  # request, return only the necessary headers and return an empty
-  # text/plain.
-  def cors_preflight_check
-    headers['Access-Control-Allow-Origin'] = set_access_control_header
-    headers['Access-Control-Allow-Methods'] = 'POST'
-    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
-    headers['Access-Control-Max-Age'] = '1728000' # in seconds ..48hours (60*60*48)
-  end
+  before_action :set_access_control_header
 
   def set_access_control_header
-    check_list_header_array = ['http://localhost:3000', '*.openhub.net']
-    check_list_header_array.include?(request.headers['origin']) ? request.headers['origin'] : false
+    headers['Access-Control-Allow-Origin'] = 'http://oh-stage-web-1.blackducksoftware.com'
   end
 
   def create
