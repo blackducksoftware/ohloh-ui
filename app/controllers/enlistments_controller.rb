49e583e26364838d81a3e92b4a986f8909b20414
santhanakarthikeyan <santhanakarthikeyan@hotmail.com>
2016-03-02 10:32:41 +0530

    OTWO-4017: Avoid duplicate repositories getting created

--- app/controllers/enlistments_controller.rb
+++ app/controllers/enlistments_controller.rb
@@ -84,7 +84,7 @@ class EnlistmentsController < SettingsController
   end
 
   def save_or_update_repository
-    @project_has_repo_url = @project.enlistments.with_repo_url(params[:repository][:url].strip).exists?
+    @project_has_repo_url = @project.enlistments.with_repo_url(@repository.url).exists?
     existing_repo = @repository_class.find_existing(@repository)
     if existing_repo.present?
       existing_repo.update_attributes(username: @repository.username, password: @repository.password)
