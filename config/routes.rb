6edc6d04cba334ead818434e40337b07e50e3d2c
Daniel Rubio <drubio1989@gmail.com>
2016-04-20 21:27:35 -0400

    OTWO-4123 Alter manual verification url from post to get

--- config/routes.rb
+++ config/routes.rb
@@ -102,7 +102,7 @@ Rails.application.routes.draw do
 
     resources :accesses, only: [], module: :accounts do
       collection do
-        post :manual_verification
+        get :manual_verification
         post :make_spammer
         get :activate
       end
