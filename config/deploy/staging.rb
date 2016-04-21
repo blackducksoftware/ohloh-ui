562214fbbc94a53eafb8b6f47fbca3d9febc451b
Alex Johnson <notalexjohnson@gmail.com>
2016-04-21 14:00:33 +0530

    Use complete addresses for staging servers

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,9 @@
-role :web_1, %w(deployer@oh-stage-web-1)
-role :web_2, %w(deployer@oh-stage-web-2)
-role :web_3, %w(deployer@oh-stage-web-3)
+role :web_1, %w(deployer@oh-stage-web-1.blackducksoftware.com)
+role :web_2, %w(deployer@oh-stage-web-2.blackducksoftware.com)
+role :web_3, %w(deployer@oh-stage-web-3.blackducksoftware.com)
 role :web_5, %w(core@10.1.1.106)
-role :web_7, %w(deployer@oh-stage-web-7)
-role :db, %w(deployer@oh-stage-utility-2), primary: true
+role :web_7, %w(deployer@oh-stage-web-7.blackducksoftware.com)
+role :db, %w(deployer@oh-stage-utility-2.blackducksoftware.com), primary: true
 
 # All passenger_roles get a deploy:restart after deploy:publishing.
 set :passenger_roles, [:web, :web_1, :web_2, :web_3, :web_5, :web_7]
