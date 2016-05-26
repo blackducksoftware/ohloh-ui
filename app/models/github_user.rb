613d006400c978155546484358d4e1cdc540c6eb
Peter Degen-Portnoy <pdegenportnoy@blackducksoftware.com>
2016-05-26 06:46:47 -0400

    Small fix to address rubocop failure.
    Rubocop does not like the semicolon style. It then complained about
    encoding.  Upgrading Rubocop has opened a Pandora's Box.
    
    The author wants this fix in production.  Damn the torpeos.  Full speed
    ahead.

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -44,7 +44,10 @@ class GithubUser
 
   def create_repositories
     urls = fetch_repository_urls
-    @repositories ||= urls.map { |url| (repo = GitRepository.find_or_create_by(url: url, branch_name: branch_name)); repo if repo.valid? }.compact!
+    @repositories ||= urls.map do |url|
+      repo = GitRepository.find_or_create_by(url: url, branch_name: branch_name)
+      repo if repo.valid?
+    end.compact!
   end
 
   def fetch_repository_urls
