abcf5b20464a79b9bf0c3a8e664d64da6917b000
Daniel Rubio <drubio1989@gmail.com>
2016-06-07 13:41:13 -0400

    Deploy Attempt #1 Initial fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.4.0'
 
 set :whoami, `whoami`.strip
-set :default_env, 'PATH' => '/home/deployer/.rbenv/shims:$PATH', 'BASH_ENV' => '/home/deployer/.production_vars'
+set :default_env, 'PATH' => '/home/deployer/.rbenv/shims:$PATH'
 
 set :application, 'openhub'
 set :repo_url, 'git@github.com:blackducksw/ohloh-ui.git'
@@ -9,7 +9,7 @@ set :user, 'deployer'
 set :use_sudo, false
 set :passenger_restart_with_sudo, false
 set :branch, ENV['branch'] || :master
-role :reverification_server, ['deployer@prd-oh-web02.dc2.lan']
+role :reverification_server, ['deployer@oh-stage-web-7.blackducksoftware.com']
 
 set :deploy_to, "/var/local/#{fetch(:application)}"
 
@@ -19,7 +19,7 @@ set :copy_exclude, ['.git']
 
 # Defaults to false. If true, it's skip migration if files in db/migrate not modified
 set :conditionally_migrate, true
-set :whenever_command, 'RAILS_ENV=production bundle exec whenever'
+set :whenever_command, 'bundle exec whenever'
 set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
 set :whenever_roles, -> { :reverification_server }
 
