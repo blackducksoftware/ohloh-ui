7272d2351123bf63053636042489dc5ea4caf37f
Daniel Rubio <drubio1989@gmail.com>
2016-06-07 15:26:26 -0400

    Remove cron automation from master

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,8 +19,5 @@ set :copy_exclude, ['.git']
 
 # Defaults to false. If true, it's skip migration if files in db/migrate not modified
 set :conditionally_migrate, true
-set :whenever_command, 'RAILS_ENV=production bundle exec whenever'
-set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
-set :whenever_roles, -> { :reverification_server }
 
 before 'deploy:check:linked_files', 'deploy:update_configuration'
