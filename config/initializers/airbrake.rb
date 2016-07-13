49f212999e7528d8bc20f51692a021950f7a061d
Paddy <rapbhan@gmail.com>
2016-07-13 09:45:14 +0530

    OTWO-4304 Fixed the AIRBRAKE PROJECT ID

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,7 +1,7 @@
 Airbrake.configure do |config|
   config.project_key = ENV['AIRBRAKE_API_KEY']
   config.host = "#{ENV['AIRBRAKE_HOST']}:#{ENV['AIRBRAKE_PORT'].to_i}"
-  config.project_id = ENV['AIRBRAKE_API_KEY']
+  config.project_id = ENV['AIRBRAKE_PROJECT_ID']
   config.ignore_environments = %w(development test)
 end
 
