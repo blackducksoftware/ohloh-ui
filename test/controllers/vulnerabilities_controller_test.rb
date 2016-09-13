dd3a5da7afd161ed89ac49d2dfe82715c58c4638
Daniel Rubio <drubio1989@gmail.com>
2016-09-12 23:23:07 -0500

    OTWO-4351 Complete Version and Time filtering

--- test/controllers/vulnerabilities_controller_test.rb
+++ test/controllers/vulnerabilities_controller_test.rb
@@ -6,9 +6,9 @@ describe 'VulnerabilitiesControllerTest' do
   before do
     project.editor_account = create(:admin)
     project.update!(best_project_security_set_id: create(:project_security_set, project: project).id)
-    project.best_project_security_set.releases << create_list(:release, 20)
+    project.best_project_security_set.releases << create_list(:release, 20, project_security_set: nil)
     project.best_project_security_set.releases.each do |r|
-      r.vulnerabilities << create(:vulnerability)
+      r.vulnerabilities << create(:vulnerability, release: nil)
     end
   end
 
@@ -17,6 +17,14 @@ describe 'VulnerabilitiesControllerTest' do
       get :version_chart, id: project.id, xhr: true
       assert_response :success
       assigns(:releases).must_equal(project.best_project_security_set.releases.last(10))
+    end
+  end
+
+  describe 'index' do
+    it 'should return all release data from oldest to newest' do
+      get :index, project_id: project.id, xhr: true
+      assert_response :success
+      assigns(:releases).must_equal(project.best_project_security_set.releases.order(released_on: :asc))
     end
   end
 end
