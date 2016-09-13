2181b2cfab3672993dedbe32db93279eb782d2f8
Daniel Rubio <drubio1989@gmail.com>
2016-09-12 23:23:07 -0500

    OTWO-4351 Complete Version and Time filtering

--- test/factories/releases.rb
+++ test/factories/releases.rb
@@ -2,8 +2,26 @@ FactoryGirl.define do
   factory :release do
     sequence :kb_release_id
     sequence :version do |n|
-      "V#{n}"
+      "#{n}.#{n}.#{n}"
     end
-    released_on { Time.now.utc }
+    released_on { Faker::Date.between(6.years.ago, Time.zone.today) }
+  end
+
+  factory :major_release_one, parent: :release do
+    sequence :version do |n|
+      "1.#{n}.#{n}"
+    end
+  end
+
+  factory :major_release_two, parent: :release do
+    sequence :version do |n|
+      "2.#{n}.#{n}"
+    end
+  end
+
+  factory :major_release_three, parent: :release do
+    sequence :version do |n|
+      "3.#{n}.#{n}"
+    end
   end
 end
--- test/factories/vulnerabilities.rb
+++ test/factories/vulnerabilities.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :vulnerability do
     sequence :cve_id
+    published_on { Faker::Date.backward(14) }
+    generated_on { Faker::Date.backward(14) }
+    score 1.0
+    severity 'low'
   end
 end
