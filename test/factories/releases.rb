ced372b1215cf82a4f86a06a8609578ebaadf5e9
Jebin <jebin,geosil@gmail.com>
2016-09-14 11:44:26 +0530

    OTWO-4398 Applied fluid layout

--- test/factories/releases.rb
+++ test/factories/releases.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :release do
     association :project_security_set
-    release_id { Faker::Lorem.word }
+    release_id
   end
+  sequence(:release_id) { |n| "#{Faker::Lorem.word}-#{n}" }
 end
