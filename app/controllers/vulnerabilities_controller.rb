b9ee994a7fb7f7ae63d208788f6bd295733303a3
Jebin <jebin,geosil@gmail.com>
2016-10-05 16:31:35 +0530

    OTWO-4415 fix test cases

--- app/controllers/vulnerabilities_controller.rb
+++ app/controllers/vulnerabilities_controller.rb
@@ -1,5 +1,6 @@
 class VulnerabilitiesController < ApplicationController
   layout 'responsive_project_layout', only: [:index]
+  helper ProjectsHelper
 
   include VulnerabilityFilters, VulnerabilitiesHelper
 
