9dece813dfd1625d5c6d521f84059c70b47d183f
Jebin <jebin,geosil@gmail.com>
2016-10-10 16:32:29 +0530

    OTWO-4310 Create Project Badges page

---
+++ app/controllers/project_badges_controller.rb
@@ -0,0 +1,52 @@
+class ProjectBadgesController < ApplicationController
+  before_action :set_project_or_fail
+  before_action :session_required, :redirect_unverified_account, only: [:create, :destroy]
+  before_action :set_project_editor_account_to_current_user
+  before_action :check_project_authorization, except: [:index]
+  before_action :project_context, only: [:index, :create]
+  before_action :set_badges, only: [:index, :create]
+  [CiiBadge, TravisBadge] if Rails.env == 'development'
+
+  helper ProjectsHelper
+  layout 'responsive_project_layout'
+
+  def index
+    @project_badge = ProjectBadge.new
+  end
+
+  def create
+    @project_badge = @project.project_badges.find_or_initialize_by(badge_params)
+    if @project_badge && @project_badge.valid?
+      save_and_redirect_valid_badge
+    else
+      flash[:warning] = 'Badge cannot be created.'
+      render :index
+    end
+  end
+
+  def destroy
+    @project_badge = ProjectBadge.find(params[:id])
+    @project_badge.deleted = true
+    @project_badge.save
+    flash[:success] = 'Badge deleted successfully.'
+    redirect_to project_project_badges_path
+  end
+
+  private
+
+  def save_and_redirect_valid_badge
+    @project_badge.deleted = false
+    @project_badge.save
+    flash[:success] = 'Badge created successfully.'
+    redirect_to project_project_badges_path
+  end
+
+  def badge_params
+    (params.require(:project_badge) || params.require(:cii_badge) || params.require(:travis_badge))
+      .permit(:repository_id, :type, :url)
+  end
+
+  def set_badges
+    @badges = ProjectBadge.subclasses.map { |b| [b.method(:badge_name).call, b.name] }
+  end
+end
--- app/models/cii_badge.rb
+++ app/models/cii_badge.rb
@@ -1,3 +1,11 @@
 class CiiBadge < ProjectBadge
   validates :url, numericality: { only_integer: true }
+
+  def badge_image
+    "https://bestpractices.coreinfrastructure.org/projects/#{url}/badge"
+  end
+
+  def self.badge_name
+    'Core Infrastructure Initiative'
+  end
 end
