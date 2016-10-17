c1d787a6f624fcf17d862f8ccb9b74c82ea37904
Jebin <jebin,geosil@gmail.com>
2016-10-17 15:09:32 +0530

    OTWO-4310 routes fix

--- app/controllers/project_badges_controller.rb
+++ app/controllers/project_badges_controller.rb
@@ -1,6 +1,11 @@
 class ProjectBadgesController < ApplicationController
   before_action :set_project_or_fail
-  before_action :show_permissions_alert, only: [:create, :destroy]
+  before_action :session_required, :redirect_unverified_account, only: [:create,:destroy]
+  before_action :set_project_editor_account_to_current_user
+  before_action :check_project_authorization, except: [:index]
+  before_action :find_enlistment, only: [:show, :edit, :update, :destroy]
+  before_action :project_context, only: [:index, :new, :edit, :create, :update]
+
   [CiiBadge, TravisBadge] if Rails.env == 'development'
 
   helper ProjectsHelper
