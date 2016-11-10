3b4551bf5ddb997529f987b130d8d5f54f8cee89
Paddy <rapbhan@gmail.com>
2016-11-10 14:53:20 +0530

    OTWO-4544 Added the Post Notification back

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,7 @@ class PostsController < ApplicationController
   def create
     @post = build_new_post
     if verify_captcha_for_non_admin && @post.save
+      post_notification(@post)
       redirect_to topic_path(@topic), notice: t('.create')
     else
       @posts = @topic.posts.paginate(page: page_param, per_page: TopicDecorator::PER_PAGE)
@@ -93,4 +94,34 @@ class PostsController < ApplicationController
     return true if current_user_is_admin?
     verify_recaptcha(model: @post, attribute: :captcha)
   end
+
+  def post_notification(post)
+    @user_who_began_topic = post.topic.account
+    @user_who_replied = post.account
+    @topic = post.topic
+    find_collection_of_users(post)
+    unless @user_who_replied != @user_who_began_topic
+      rejected = @all_users_preceding_the_last_user.reject { |user| user.id == @user_who_replied.id }
+      @all_users_preceding_the_last_user = rejected
+    end
+    send_reply_emails_to_everyone
+    send_creation_email if @user_who_replied.email_topics?
+   end
+
+   def find_collection_of_users(post)
+     @all_users_preceding_the_last_user = post.topic.posts.map(&:account).select(&:email_topics?)
+     @all_users_preceding_the_last_user.pop unless @all_users_preceding_the_last_user.one?
+     @all_users_preceding_the_last_user
+   end
+
+   def send_reply_emails_to_everyone
+     @all_users_preceding_the_last_user.uniq.each do |user|
+       PostNotifier.post_replied_notification(user, @user_who_replied, @post).deliver_now
+     end
+   end
+
+   def send_creation_email
+     PostNotifier.post_creation_notification(@user_who_replied, @topic).deliver_now
+   end
+
 end
---
+++ app/mailers/post_notifier.rb
@@ -0,0 +1,30 @@
+class PostNotifier < ActionMailer::Base
+  default from: 'mailer@blackducksoftware.com'
+
+  def post_creation_notification(user_who_replied, topic)
+    @user_who_replied = user_who_replied
+    @topic = topic
+    @unsubscribe_emails_url = unsubscribe_emails_accounts_url(
+      notification_type: 'post',
+      key: Account::Subscription.new(@user_who_replied).generate_unsubscription_key
+   )
+
+    mail(to: @user_who_replied.email,
+         subject: t('.subject'),
+         template_path: 'mailers',
+         template_name: 'post_notifier')
+  end
+
+  def post_replied_notification(user, user_who_replied, post)
+    @user_who_needs_reply = user
+    @user_who_replied = user_who_replied
+    @topic = post.topic
+    @post = post
+    @unsubscribe_emails_url =
+      unsubscribe_emails_accounts_url(notification_type: 'post',
+                                     key: Account::Subscription.new(@user_who_needs_reply).generate_unsubscription_key)
+
+    mail(to: @user_who_needs_reply.email, subject: t('.subject'),
+        template_path: 'mailers', template_name: 'reply_notifier')
+  end
+end
