a76a5d0681356ce5708d7a12f7b048491e1b1e33
Paddy <rapbhan@gmail.com>
2016-11-29 14:22:41 +0530

    Removed the Duplication/Un-used code to improve rcov coverage

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,7 +1,4 @@
 module TagsHelper
-  def tag_icon
-    '<span class="icon-tag"></span>'.html_safe
-  end
 
   def tag_icon_link(project)
     link_to_if project.edit_authorized?, t('.tags'), project_tags_path(project), class: 'noborder'
