7a3ebeb5b6b3ceb60bbd68983462471de5c80a8d
Alex Johnson <notalexjohnson@gmail.com>
2017-05-22 17:00:06 +0530

    OTWO-4772 Update readme to reflect current installation method

---
+++ script/setup_default_admin.rb
@@ -0,0 +1,10 @@
+require_relative '../config/environment'
+
+login = ARGV[0] || :admin_user
+password = ARGV[1] || :admin_password
+email = ARGV[2] || 'admin@example.com'
+
+account = Account.create!(login: login, level: 10, activated_at: Time.current,
+                          email: email, email_confirmation: email,
+                          password: password, password_confirmation: password)
+ManualVerification.create!(account: account, auth_id: account.login)
