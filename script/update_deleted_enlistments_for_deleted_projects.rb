4c78892fe88c866882898aba928f5da9fd6d8f1f
Matthew Krasnick <matthew.krasnick@gmail.com>
2017-06-05 15:26:50 -0400

    Code Review changes

--- script/update_deleted_enlistments_for_deleted_projects.rb
+++ script/update_deleted_enlistments_for_deleted_projects.rb
@@ -4,22 +4,32 @@ raise 'RAILS_ENV is undefined' unless ENV['RAILS_ENV']
 
 require_relative '../config/environment'
 require 'logger'
-$LOG = Logger.new('update_deleted_enlistments_log_file.log')
 
-# update enlistments deleted field for all deleted projects
-puts 'starting script'
-editor = Account.find_by_login('ohloh_slave')
-Project.where(deleted: true).find_in_batches do |projects|
-  projects.each do |project|
-    begin
-      puts "Processing project #{project.id}"
-      project.enlistments.each do |enlistment|
-        enlistment.create_edit.undo!(editor) if enlistment.create_edit.allow_undo?
+class UpdateEnlistmentsForDeletedProjects
+  def initialize
+    @log = Logger.new('update_deleted_enlistments_log_file.log')
+    @editor = Account.find_by_login('ohloh_slave')
+  end
+
+  def execute
+    puts 'starting script'
+    
+    # update enlistments deleted field for all deleted projects
+    Project.where(deleted: true).find_in_batches do |projects|
+      projects.each do |project|
+        begin
+          puts "Processing project #{project.id}"
+          project.enlistments.each do |enlistment|
+            enlistment.create_edit.undo!(@editor) if enlistment.create_edit.allow_undo?
+          end
+        rescue Exception => e
+          @log.error "error: #{project.id} - #{e.inspect}"
+        end
       end
-    rescue Exception => e
-      $LOG.error  "error: #{project.id} - #{e.inspect}"
     end
+    puts 'Successfully completed script - Please check update_deleted_enlistments_log_file.log to view any exceptions'
   end
 end
-puts 'Successfully completed script - Please check update_deleted_enlistments_log_file.log to view any exceptions'
+
+UpdateEnlistmentsForDeletedProjects.new.execute
 
