94ec56f30cf715b50d46f97b29f06ebab93481d2
Matthew Krasnick <matthew.krasnick@gmail.com>
2017-06-05 12:14:24 -0400

    OTWO-4789 - pull request changes

--- script/update_deleted_enlistments_for_deleted_projects.rb
+++ script/update_deleted_enlistments_for_deleted_projects.rb
@@ -3,18 +3,23 @@
 raise 'RAILS_ENV is undefined' unless ENV['RAILS_ENV']
 
 require_relative '../config/environment'
+require 'logger'
+$LOG = Logger.new('update_deleted_enlistments_log_file.log')
 
 # update enlistments deleted field for all deleted projects
 puts 'starting script'
 editor = Account.find_by_login('ohloh_slave')
 Project.where(deleted: true).find_in_batches do |projects|
   projects.each do |project|
-    project.enlistments.each do |enlistment|
-      enlistment.create_edit.undo!(editor) if enlistment.create_edit.allow_undo?
+    begin
+      puts "Processing project #{project.id}"
+      project.enlistments.each do |enlistment|
+        enlistment.create_edit.undo!(editor) if enlistment.create_edit.allow_undo?
+      end
+    rescue Exception => e
+      $LOG.error  "error: #{project.id} - #{e.inspect}"
     end
-    puts 'finished updating enlistments'
   end
-  puts 'finished script'
 end
-
+puts 'Successfully completed script - Please check update_deleted_enlistments_log_file.log to view any exceptions'
 
