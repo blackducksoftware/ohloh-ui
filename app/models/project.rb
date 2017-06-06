d72e7dc7ffb428789545cb0a96bde4a5764678c7
msk999 <msk999@users.noreply.github.com>
2017-06-06 17:10:18 -0400

    OTWO-4789 Rolling back change to remove_enlistments
    Rolling back change to remove_enlistments to allow for further testing

--- app/models/project.rb
+++ app/models/project.rb
@@ -20,7 +20,7 @@ class Project < ActiveRecord::Base
   after_save :update_organzation_project_count
   after_update :remove_people, if: -> (project) { project.deleted_changed? && project.deleted? }
   after_update :recalc_tags_weight!, if: -> (project) { project.deleted_changed? }
-  after_update :remove_enlistments, if: -> (project) { project.deleted_changed? && project.deleted? }
+  #after_update :remove_enlistments, if: -> (project) { project.deleted_changed? && project.deleted? }// msk rolled back change
 
   attr_accessor :managed_by_creator
 
