ce1ea4506ad7ffc5cc9c61ded18a323ae013d298
santhanakarthikeyan <santhanakarthikeyan@hotmail.com>
2017-06-21 11:38:58 +0530

    OTWO-4822: Cleanup invalid Git repositories

---
+++ script/validate_git_repositories.rb
@@ -0,0 +1,19 @@
+#! /usr/bin/env ruby
+
+require_relative '../config/environment'
+
+index = 0
+CodeLocation.joins(:repository, :projects)
+            .where(enlistments: { deleted: false })
+            .where(projects: { deleted: false })
+            .where(repositories: { type: 'GitRepository' })
+            .find_each do |cl|
+  puts "#{cl.id} :: #{index += 1}"
+  cl.enlistments do |e|
+    begin
+      e.create_edit.undo!(Account.hamster) unless cl.valid?
+    rescue
+      nil
+    end
+  end
+end
