a92d35b875a4d5c3ec78285b824ffd2f8bfc9e7b
Jebin <jebin.geosil@gmail.com>
2017-06-27 15:40:28 +0530

    OTWO-4821 Fix codelocation edit issue

---
+++ test/controllers/admin/code_locatons_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+describe 'Admin::CodeLocationsController' do
+  let(:admin) { create(:admin) }
+  let(:project) { create(:project) }
+  let(:code_location) { create(:code_location) }
+  before do
+    login_as admin
+  end
+
+  describe '#index' do
+    it 'should render index template' do
+      get :index
+      must_respond_with :ok
+      must_render_template :index
+    end
+  end
+
+  describe '#show' do
+    it 'should render show page when code location is passd' do
+      get :show, id: code_location
+      must_respond_with :ok
+      must_render_template :show
+    end
+  end
+
+  describe '#edit' do
+    it 'should render edit form when valid code_location is passesd' do
+      get :edit, id: code_location
+      must_respond_with :ok
+      must_render_template :edit
+    end
+  end
+end
