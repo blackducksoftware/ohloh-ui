f1e127c42ed6b29bf774fd82bc0a31e5d8195b12
Peter Degen-Portnoy <pdegenportnoy@blackducksoftware.com>
2017-09-11 16:19:22 -0400

    Improve License Permission Role Filters

--- app/admin/license_permission_role.rb
+++ app/admin/license_permission_role.rb
@@ -1,6 +1,11 @@
 ActiveAdmin.register LicensePermissionRole do
   permit_params :status, :license_id, :license_permission_id
 
+  filter :license, collection: proc {License.order(:name)}
+  filter :license_permission, collection: proc {LicensePermission.order(:name)}
+  # filter :status, as: :select, collection: ['permitted', 'forbidden', 'required']
+  filter :status
+
   index do
     column :id
     column :license_id
