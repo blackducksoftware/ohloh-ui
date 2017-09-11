bf35738363696538d1609ab678aa0a09f7483b9e
Peter Degen-Portnoy <pdegenportnoy@blackducksoftware.com>
2017-09-11 14:00:22 -0400

    Minor UI Tweaks to License Permission Roles Admin page

--- app/admin/license_permission_role.rb
+++ app/admin/license_permission_role.rb
@@ -1,3 +1,20 @@
 ActiveAdmin.register LicensePermissionRole do
   permit_params :status, :license_id, :license_permission_id
+
+  index do
+    column :id
+    column :license_id
+    column :license_permission_id
+    column :status
+    actions
+  end
+
+  form do |f|
+    f.inputs 'License Permiission Role' do
+      f.input :license, as: :select, collection: License.select(:name).order(:name)
+      f.input :license_permission, as: :select
+      f.input :status
+      actions
+    end
+  end
 end
