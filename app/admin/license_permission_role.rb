d98027c781ad8367da1fabdd78a1ac8d98389765
Peter Degen-Portnoy <pdegenportnoy@blackducksoftware.com>
2017-09-12 17:28:14 -0400

    Fix select so that License Permission Role can be added

--- app/admin/license_permission_role.rb
+++ app/admin/license_permission_role.rb
@@ -16,10 +16,14 @@ ActiveAdmin.register LicensePermissionRole do
 
   form do |f|
     f.inputs 'License Permiission Role' do
-      f.input :license, as: :select, collection: License.select(:name).order(:name)
+      f.input :license, as: :select, collection: License.order(:name)
       f.input :license_permission, as: :select
       f.input :status
       actions
     end
+  end
+
+  action_item :add_another_new, only: :show do
+    link_to "Add another License Permission role",  new_admin_license_permission_role_path
   end
 end
