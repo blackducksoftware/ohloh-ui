d7cf1c332122ec0659c8ff1465e5c7d6c02d5927
Daniel Rubio <drubio1989@gmail.com>
2017-09-18 13:14:07 -0500

    Add Rack mini profiler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,11 @@ class ApplicationController < ActionController::Base
   before_action :strip_query_param
   before_action :clear_reminder
   before_action :verify_api_access_for_xml_request, only: [:show, :index]
+  before_action do
+    if current_user && current_user.is_admin?
+      Rack::MiniProfiler.authorize_request
+    end
+  end
 
   def initialize(*params)
     @page_context = {}
