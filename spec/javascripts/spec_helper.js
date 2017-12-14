a91cc76d11980afdc9f7869f5e3499cbe5cc088c
Alex Johnson <notalexjohnson@gmail.com>
2017-12-15 04:58:58 +0530

    Remove unused digits references

--- spec/javascripts/spec_helper.js
+++ spec/javascripts/spec_helper.js
@@ -8,7 +8,6 @@
 // PhantomJS (Teaspoons default driver) doesn't have support for Function.prototype.bind, which has caused confusion.
 // Use this polyfill to avoid the confusion.
 //= require support/phantomjs-shims
-//= require support/digits
 //= require application
 //
 // Deferring execution
