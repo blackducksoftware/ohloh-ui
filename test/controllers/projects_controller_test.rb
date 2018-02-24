6531afdb4cb2fe4b4f9f4ccd8acf54716acedfb0
Paddy <rapbhan@gmail.com>
2018-02-24 11:20:05 +0530

    OTWO-5095 Fixed the Project Jobs Link in Project show page

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -385,7 +385,7 @@ describe 'ProjectsController' do
       login_as create(:admin)
       get :show, id: project
       must_respond_with :ok
-      assert_select "a[href='#{admin_project_jobs_path(project)}']", text: /View Jobs/
+      assert_select "a[href='#{oh_admin_project_jobs_path(project)}']", text: /View Jobs/
     end
 
     it 'should not show the jobs link for non-admins' do
@@ -393,7 +393,7 @@ describe 'ProjectsController' do
       login_as create(:account)
       get :show, id: project
       must_respond_with :ok
-      assert_select "a[href='#{admin_project_jobs_path(project)}']", false, text: /View Jobs/
+      assert_select "a[href='#{oh_admin_project_jobs_path(project)}']", false, text: /View Jobs/
     end
 
     it 'must render 404 if unknown format' do
