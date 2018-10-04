780d58b8287507a040add7393585aaad79b36706
Paddy <rapbhan@gmail.com>
2018-10-04 16:44:48 +0530

    OTWO-5270 Added the schedule to run the rake tasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,4 @@
-set :output, error: 'reverification_cron_error.log', standard: 'reverification_cron.log'
-
-job_type :thor_command, %{export PATH=$HOME/.rbenv/shims:$HOME/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
-                          cd /var/local/openhub/current && RAILS_ENV=production bundle exec thor :task }
-
-every 1.day, at: '9:00 am' do
-  thor_command 'reverification_task:reverify:execute --bounce_threshold 30 --num_email 2000'
+set :environment, 'production'
+every 2.days, at: '11.00 am', roles: [:sidekiq] do
+  rake check_broken_links
 end
