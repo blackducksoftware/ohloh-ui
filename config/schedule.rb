041797dbdf2850501e719d934737f470977a6292
Alex Johnson <notalexjohnson@gmail.com>
2019-01-13 16:35:03 +0530

    OTWO-5428 Configure whenever with capistrano

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 set :environment, 'production'
 set :output, error: 'cron_error.log', standard: 'cron.log'
-every 2.weeks, at: '11.00 am', roles: [:sidekiq] do
+every 2.weeks, at: '11.00 am', roles: [:utility] do
   rake 'check_broken_links'
 end
 
-every 1.day, at: '04.00 am', roles: [:sidekiq] do
+every 1.day, at: '04.00 am', roles: [:utility] do
   rake 'home_page_stats'
 end
