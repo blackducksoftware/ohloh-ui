12b3c4ccb79718d40b4fe9166c03d9397534177b
Peter Degen-Portnoy <pdegenportnoy@blackducksoftware.com>
2019-01-13 21:01:09 -0500

    Restore old sidekiq role for DB migratino

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,9 @@
 role :web, ['deployer@prd-oh-web01.dc2.lan', 'deployer@prd-oh-web02.dc2.lan',
             'deployer@prd-oh-web03.dc2.lan']
 
-role :utility, 'serv-deployer@prd-oh-utility03.dc2.lan', user: 'serv-deployer'
+# role :utility, 'serv-deployer@prd-oh-utility03.dc2.lan', user: 'serv-deployer'
 
+role :sidekiq, %w[deployer@prd-oh-utility01.dc2.lan]
 set :sidekiq_role, [:utility]
 set sidekiq_env: fetch(:rails_env)
 
