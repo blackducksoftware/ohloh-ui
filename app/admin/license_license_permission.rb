7e9538d1b79b9030072f7fb0405751cdd4d8fcc2
Matthew Krasnick <matthew.krasnick@gmail.com>
2019-02-01 14:14:15 -0500

    OTWO-5465 - rubocop changes

--- app/admin/license_license_permission.rb
+++ app/admin/license_license_permission.rb
@@ -1,34 +1,29 @@
-ActiveAdmin.register LicenseLicensePermission, as: "license_permission" do
-    permit_params :status, :license_id, :license_permission_id
+ActiveAdmin.register LicenseLicensePermission, as: 'license_permission' do
+  permit_params :status, :license_id, :license_permission_id
 
-    filter :license, collection: proc { License.active.order(:name) }
-    # filter :license_permission, collection: proc { LicenseRight.order(:name) }
-    # filter :status, as: :select, collection: [['Permitted', 0],
-    #                                         ['Forbidden', 1],
-    #                                         ['Required', 2]]
+  filter :license, collection: proc { License.active.order(:name) }
+  # filter :license_permission, collection: proc { LicenseRight.order(:name) }
 
-    index do
-      column :id
-      column 'License' do |permission|
-        permission.license.name
-      end
-      column 'License Permission' do |permission|
-        permission.name
-      end
-      column :status
+  index do
+    column :id
+    column 'License' do |permission|
+      permission.license.name
+    end
+    column 'License Permission', :name
+    column :status
+    actions
+  end
+
+  form do |f|
+    f.inputs 'License Permissions' do
+      f.input :license, as: :select, collection: License.order(:name)
+      f.input :license_permission, as: :select
+      # f.input :status, as: :select, collection: LicensePermission.statuses.keys
       actions
     end
-
-    form do |f|
-      f.inputs 'License Permissions' do
-        f.input :license, as: :select, collection: License.order(:name)
-        f.input :license_permission, as: :select
-        # f.input :status, as: :select, collection: LicensePermission.statuses.keys
-        actions
-      end
-    end
-
-    action_item :add_another_new, only: :show do
-      link_to 'Add another License Permission', new_admin_license_permission_role_path
-    end
   end
+
+  action_item :add_another_new, only: :show do
+    link_to 'Add another License Permission', new_admin_license_permission_role_path
+  end
+end
--- app/models/license_license_permission.rb
+++ app/models/license_license_permission.rb
@@ -1,7 +1,7 @@
 class LicenseLicensePermission < ActiveRecord::Base
   belongs_to :license
   belongs_to :license_permission
-  delegate :name, :icon, :status, to: :license_permission if :license_permission
+  delegate :name, :icon, :status, to: :license_permission
   validates :license_permission, presence: true
   validates :license, presence: true
 end
