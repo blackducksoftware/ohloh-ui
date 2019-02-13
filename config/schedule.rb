602ab7f475c3d29c8277e834a313f50c293c13c7
Priya Rani <prani@blackducksoftware.com>
2019-02-13 18:46:37 +0530

    Update role to sidekiq

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 set :environment, 'production'
 set :output, error: 'cron_error.log', standard: 'cron.log'
-every 2.weeks, at: '11.00 am', roles: [:utility] do
+every 2.weeks, at: '11.00 am', roles: [:sidekiq] do
   rake 'check_broken_links'
 end
 
-every 1.day, at: '04.00 am', roles: [:utility] do
+every 1.day, at: '04.00 am', roles: [:sidekiq] do
   rake 'home_page_stats'
 end
