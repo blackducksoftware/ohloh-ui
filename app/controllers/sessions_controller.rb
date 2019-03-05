7473e3a60ae61f43d7a7c1662e59e262fcab391a
Alex Johnson <notalexjohnson@gmail.com>
2019-03-05 17:11:16 +0530

    OTWO-5503 Add recaptcha before final auth failure

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,18 @@
 class SessionsController < Clearance::SessionsController
+  before_action :account_must_exist, only: :create
+  before_action :captcha_verify, if: :last_allowed_login_attempt?
+  before_action :reset_auth_fail_count, only: :create, if: :auth_failure_timeout?
+  attr_reader :account
+
   def create
-    @account = Account.fetch_by_login_or_email(params[:login][:login])
     account_or_nil = authenticate(params)
 
     sign_in(account_or_nil) do |status|
       if status.success?
-        @account.update!(auth_fail_count: 0)
+        reset_auth_fail_count
         redirect_back_or url_after_create
       else
+        increment_auth_fail_count
         sign_in_failure(status.failure_message)
       end
     end
@@ -15,38 +20,57 @@ class SessionsController < Clearance::SessionsController
 
   private
 
+  def last_allowed_login_attempt?
+    retries_remaining <= 1
+  end
+
+  def captcha_verify
+    return if verify_recaptcha
+    @ask_for_recaptcha = true
+    flash.now[:error] = t('.recaptcha_failure')
+    render 'sessions/new', status: :unauthorized
+  end
+
   def sign_in_failure(failure_message)
-    set_auth_fail_count
-
-    if retries_remaining >= 1
-      flash.now.notice = failure_message
-    else
-      disable_account_and_notify_admin
-      flash.now.notice = t('.locked_message')
-    end
-
-    render template: 'sessions/new', status: :unauthorized
+    flash.now[:error] = failure_message
+    @ask_for_recaptcha = true if retries_remaining == 1
+    disable_account_for_retries
+    render 'sessions/new', status: :unauthorized
   end
 
-  def set_auth_fail_count
-    if auth_failure_within_lock_timeout?
-      @account.update!(auth_fail_count: @account.auth_fail_count + 1)
-    else
-      @account.update!(auth_fail_count: 1)
-    end
+  def disable_account_for_retries
+    return if retries_remaining > 0 || account.access.disabled?
+    disable_account_and_notify_admin
+    flash.now[:error] = t('.locked_message')
   end
 
-  def auth_failure_within_lock_timeout?
-    Time.current - @account.updated_at < ENV['FAILED_LOGIN_TIMEOUT'].to_i.minutes
+  def auth_failure_timeout?
+    return unless account
+    Time.current - account.updated_at > ENV['FAILED_LOGIN_TIMEOUT'].to_i.minutes.to_i
   end
 
   def retries_remaining
-    ENV['MAX_LOGIN_RETRIES'].to_i - @account.auth_fail_count
+    ENV['MAX_LOGIN_RETRIES'].to_i - account.auth_fail_count
   end
 
   def disable_account_and_notify_admin
-    @account.update!(level: Account::Access::DISABLED)
-    AccountMailer.notify_disabled_account_for_login_failure(@account).deliver_now
-    NewRelic::Agent.notice_error("#{@account.login} deactivated for repeated failed login attempts.")
+    account.update!(level: Account::Access::DISABLED)
+    AccountMailer.notify_disabled_account_for_login_failure(account).deliver_now
+    NewRelic::Agent.notice_error("#{account.login} deactivated for repeated failed login attempts.")
+  end
+
+  def increment_auth_fail_count
+    account.update!(auth_fail_count: account.auth_fail_count + 1)
+  end
+
+  def reset_auth_fail_count
+    account.update!(auth_fail_count: 0)
+  end
+
+  def account_must_exist
+    @account = Account.fetch_by_login_or_email(params[:login][:login])
+    return if @account
+    flash.now[:error] = t('flashes.failure_after_create')
+    render 'sessions/new', status: :unauthorized
   end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -4,27 +4,31 @@ describe 'SessionsController' do
   describe 'create' do
     let(:password) { Faker::Internet.password }
     let(:account) { create(:account, password: password) }
+    let(:max_login_retries) { ENV['MAX_LOGIN_RETRIES'].to_i }
 
     describe 'success' do
-      before do
-        Account.any_instance.stubs(:authenticated?).returns(true)
-      end
-
       it 'must redirect to accounts/me after login' do
-        post :create, login: { login: account.login }
+        post :create, login: { login: account.login, password: password }
         must_redirect_to '/accounts/me'
       end
 
       it 'must reset auth_fail_count' do
         account.update!(auth_fail_count: 3)
-        post :create, login: { login: account.login }
+        post :create, login: { login: account.login, password: password }
         Account.find(account.id).auth_fail_count.must_equal 0
       end
     end
 
     describe 'failure' do
+      it 'must handle invalid email or login' do
+        post :create, login: { login: Faker::Name.name }
+
+        must_render_template 'sessions/new'
+        flash.now[:error].must_equal I18n.t('flashes.failure_after_create')
+      end
+
       it 'must increment auth failure count' do
-        auth_fail_count = ENV['MAX_LOGIN_RETRIES'].to_i - 2
+        auth_fail_count = max_login_retries - 2
         account.update!(auth_fail_count: auth_fail_count)
         post :create, login: { login: account.login }
 
@@ -32,21 +36,64 @@ describe 'SessionsController' do
         must_respond_with :unauthorized
         account.reload
         account.auth_fail_count.must_equal(auth_fail_count + 1)
+        flash.now[:error].must_equal I18n.t('flashes.failure_after_create')
+        refute account.access.disabled?
+      end
+
+      it 'wont compare password when recaptcha fails on the last try but auth passes' do
+        auth_fail_count = max_login_retries - 1
+        account.update!(auth_fail_count: auth_fail_count)
+        @controller.expects(:verify_recaptcha)
+
+        post :create, login: { login: account.login, password: password },
+                      'g-recaptcha-response': Faker::Internet.password
+        account.reload
+
+        must_render_template 'sessions/new'
+        account.auth_fail_count.must_equal auth_fail_count
+        flash.now[:error].must_equal I18n.t('sessions.create.recaptcha_failure')
+        refute account.access.disabled?
+      end
+
+      it 'wont compare password or disable account when recaptcha fails on the last try alongwith auth failure' do
+        auth_fail_count = max_login_retries - 1
+        account.update!(auth_fail_count: auth_fail_count)
+        @controller.expects(:verify_recaptcha)
+
+        post :create, login: { login: account.login }, 'g-recaptcha-response': Faker::Internet.password
+        account.reload
+
+        must_render_template 'sessions/new'
+        must_select('label.control-label').children.first.text.must_equal I18n.t('shared.captcha.captcha_label')
+        account.auth_fail_count.must_equal auth_fail_count
+        flash.now[:error].must_equal I18n.t('sessions.create.recaptcha_failure')
         refute account.access.disabled?
       end
 
       it 'must disable account and send notice when auth failure reaches limit' do
-        auth_fail_count = ENV['MAX_LOGIN_RETRIES'].to_i - 1
+        auth_fail_count = max_login_retries - 1
         account.update!(auth_fail_count: auth_fail_count)
 
         NewRelic::Agent.expects(:notice_error)
         AccountMailer.expects(:notify_disabled_account_for_login_failure).returns(stub(:deliver_now))
-        post :create, login: { login: account.login }
+        @controller.expects(:verify_recaptcha).returns(true)
+
+        post :create, login: { login: account.login }, 'g-recaptcha-response': Faker::Internet.password
+        account.reload
 
         must_render_template 'sessions/new'
-        account.reload
         account.auth_fail_count.must_equal(auth_fail_count + 1)
         assert account.access.disabled?
+      end
+
+      it 'must render a captcha form before the last try' do
+        account.update!(auth_fail_count: max_login_retries - 2)
+
+        post :create, login: { login: account.login }
+
+        assigns(:ask_for_recaptcha).must_equal true
+        must_render_template 'sessions/new'
+        must_select('label.control-label').children.first.text.must_equal I18n.t('shared.captcha.captcha_label')
       end
 
       it 'must reset auth_fail_count if login fails after FAILED_LOGIN_TIMEOUT' do
@@ -66,7 +113,7 @@ describe 'SessionsController' do
         post :create, login: { login: account.login, password: password }
 
         must_render_template 'sessions/new'
-        flash.now[:notice].must_equal I18n.t('accounts.disabled_error')
+        flash.now[:error].must_equal I18n.t('accounts.disabled_error')
       end
     end
   end
