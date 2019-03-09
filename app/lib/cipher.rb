d18014419a9287c7b0409dbba1947eb2fb798871
Alex Johnson <notalexjohnson@gmail.com>
2019-03-10 00:42:12 +0530

    OTWO-5487 Limit openssl key to 32 chars for compatibility
    See: https://github.com/ruby/openssl/issues/116

--- app/lib/cipher.rb
+++ app/lib/cipher.rb
@@ -8,14 +8,14 @@ module Ohloh
       def encrypt(data, key = default_key)
         aes = OpenSSL::Cipher.new('AES-256-CBC')
         aes.encrypt
-        aes.key = key
+        aes.key = key[0..31]
         CGI.escape(Base64.encode64(aes.update(data) + aes.final))
       end
 
       def decrypt(data, key = default_key)
         aes = OpenSSL::Cipher.new('AES-256-CBC')
         aes.decrypt
-        aes.key = key
+        aes.key = key[0..31]
         data = Base64.decode64(CGI.unescape(data))
         aes.update(data) + aes.final
       rescue
