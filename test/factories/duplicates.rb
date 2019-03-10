48df2e3fdc7dae36910b838aa16229b09c782069
Alex Johnson <notalexjohnson@gmail.com>
2019-03-10 17:29:10 +0530

    OTWO-5487 New FactoryGirl wont create associations on build

--- test/factories/duplicates.rb
+++ test/factories/duplicates.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :duplicate do
     association :account
-    association :good_project, factory: :project
-    association :bad_project, factory: :project
+    association :good_project, factory: :project, strategy: :create
+    association :bad_project, factory: :project, strategy: :create
     comment { Faker::Lorem.sentence }
   end
 end
--- test/factories/rss_subscriptions.rb
+++ test/factories/rss_subscriptions.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :rss_subscription do
-    association :project
+    association :project, strategy: :create
     association :rss_feed
     before(:create) { |instance| instance.editor_account = create(:admin) }
   end
