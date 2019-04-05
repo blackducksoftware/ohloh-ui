ac70d24a75b8c7b4a4e1230decbd3ac393eeb809
Alex Johnson <notalexjohnson@gmail.com>
2019-04-05 22:53:12 +0530

    OTWO-5520 Fix deployment setup for new staging servers

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,11 +1,8 @@
 role :web_1, %w[serv-deployer@oh-web01.dc1.lan]
 role :web_2, %w[serv-deployer@oh-web02.dc1.lan]
 role :web_3, %w[serv-deployer@oh-web03.dc1.lan]
-role :web_4, %w[serv-deployer@oh-web04.dc1.lan]
-role :web_5, %w[serv-deployer@oh-web05.dc1.lan]
-role :web_6, %w[serv-deployer@oh-web06.dc1.lan]
-role :web_7, %w[serv-deployer@oh-web07.dc1.lan]
-role :web_8, %w[serv-deployer@oh-web08.dc1.lan]
+
+role :web, ['serv-deployer@oh-web01.dc1.lan', 'serv-deployer@oh-web02.dc1.lan', 'serv-deployer@oh-web03.dc1.lan']
 
 role :selenium, %w[serv-deployer@oh-web03.dc1.lan]
 
@@ -27,3 +24,4 @@ set :linked_files, %w[.env.staging]
 
 set sidekiq_env: fetch(:rails_env)
 set :assets_roles, %i[web_1 web_2 web_3 web_4 web_5 web_6 web_7 web_8]
+set :passenger_restart_with_touch, true
