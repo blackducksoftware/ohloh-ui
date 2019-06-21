1117c4f76a839e114a64dc0f7f4429a2e6211da2
Peter Degen-Portnoy <pdegenportnoy@blackducksoftware.com>
2019-06-21 09:12:51 -0400

    Add another SemrushBot to filter out. Small fix to annoying image test

--- test/lib/spark/analysis_spark_test.rb
+++ test/lib/spark/analysis_spark_test.rb
@@ -12,7 +12,7 @@ class Spark::AnalysisSparkTest < ActiveSupport::TestCase
       expected_image_path = Rails.root.join('test', 'data', 'spark', 'age_spark.png')
       result_file = Spark::AnalysisSpark.new(data, max_value: 31).render
 
-      compare_images(result_file.path, expected_image_path, 0.14)
+      compare_images(result_file.path, expected_image_path, 0.17) # bumped from 0.14
     end
   end
 end
