656489af9e6b60746b5194dc5ba8b07f78f8b83c
Christopher Pallotta <chris.pallotta@gmail.com>
2019-10-28 10:54:59 -0400

    this migration shouldn't be in ohloh-ui

--- db/migrate/20190529010101_alter_slave_and_jobs_table.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AlterSlaveAndJobsTable < ActiveRecord::Migration
-  def change
-    add_column :slaves, :queue_name, :string
-    add_column :jobs, :is_expensive, :boolean, default: false, index: true
-  end
-end
