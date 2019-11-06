cbf88ef028be616cacb02ef1574d8b9d8d4b601e
Christopher Pallotta <chris.pallotta@gmail.com>
2019-11-06 17:43:42 -0500

    addressing Rails/OutputSafety where necessary

--- test/helpers/blog_link_helper_test.rb
+++ test/helpers/blog_link_helper_test.rb
@@ -8,7 +8,7 @@ class BlogLinkHelperTest < ActionView::TestCase
   describe 'blog_link_to' do
     it 'should return proper blog link' do
       BLOG_LINKS.each do |k, v|
-        link = "<a class='meta' href='http://blog.openhub.net/#{v}' target='_blank'>rest</a>".html_safe
+        link = safe_text("<a class='meta' href='http://blog.openhub.net/#{v}' target='_blank'>rest</a>")
         blog_link_to(link: k, link_text: 'rest').must_equal link
       end
     end
