1417becb93d36d55bad0a7d7417442bbc4d28d80
Paddy <rapbhan@users.noreply.github.com>
2019-11-12 12:07:54 +0530

    OTWO-5786 Updated the Docker online/offline toggle deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,23 +1,12 @@
 # frozen_string_literal: true
 
-on ['serv-deployer@prd-oh-web04.dc2.lan', 'serv-deployer@prd-oh-web05.dc2.lan', 'serv-deployer@prd-oh-web06.dc2.lan'] do
-  within '/home/serv-deployer' do
-    execute('wget -O docker-compose.yml https://raw.githubusercontent.com/blackducksoftware/ohloh-ui/master/docker-compose.yml')
-    execute('docker pull sigsynopsys/openhub:latest')
-  end
-end
+set :web_heads, ['serv-deployer@prd-oh-web04.dc2.lan', 'serv-deployer@prd-oh-web05.dc2.lan',
+                 'serv-deployer@prd-oh-web06.dc2.lan']
 
-# Run the bundle install, assets on one of the web host
-on ['serv-deployer@prd-oh-web06.dc2.lan'] do
-  within '/home/serv-deployer' do
-    execute('docker-compose run --rm web bundle install')
-    execute('docker-compose run --rm web bundle exec rake assets:precompile RAILS_ENV=production')
-  end
-end
+set :utility, 'serv-deployer@prd-oh-utility01.dc2.lan'
 
-# Rebuild the container with the latest changes (code, assets)
-on ['serv-deployer@prd-oh-web04.dc2.lan', 'serv-deployer@prd-oh-web05.dc2.lan', 'serv-deployer@prd-oh-web06.dc2.lan'] do
-  within '/home/serv-deployer' do
-    execute('docker-compose up -d --build')
-  end
+namespace :deploy do
+  task started: 'docker:deploy'
+  task offline: 'docker:offline'
+  task online:  'docker:online'
 end
