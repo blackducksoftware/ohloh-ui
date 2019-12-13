d6f7c29b6cc04fdedbe1bcd69563008b1cc2f579
Matthew Krasnick <matthew.krasnick@gmail.com>
2019-12-13 10:56:53 -0500

    Fixed Test

--- test/models/analyze_job_test.rb
+++ test/models/analyze_job_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 class ProjectAnalysisJobTest < ActiveSupport::TestCase
   describe 'progress_message' do
     it 'should return required message' do
-      job = ProjectAnalysis.create(project: create(:project))
+      job = ProjectAnalysisJob.create(project: create(:project))
       job.progress_message.must_equal "Analyzing project #{job.project.name}"
     end
   end
