088ab476a3b5e52a8cd59007d5972108ae51e1e9
kselvraj <kselvraj@blackduck.com>
2020-01-06 22:42:15 +0530

    OTWO-5855: fix encoding issue on links index page

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
+# rubocop: disable Lint/UriEscapeUnescape
+
 module LinksHelper
   def safe_slice_host(url, length = 33)
-    safe_url = sanitize(url)
+    safe_url = sanitize(URI.encode(url))
     hostname = URI.parse(safe_url).host
     truncate(hostname, length: length)
   end
 end
+# rubocop: enable Lint/UriEscapeUnescape
---
+++ test/helpers/links_helper_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class LinksHelperTest < ActionView::TestCase
+  include LinksHelper
+
+  it 'should slice host' do
+    safe_slice_host('http://test.com/value').must_equal 'test.com'
+  end
+
+  it 'should return host if url contains utf-8 encoding' do
+    safe_slice_host('http://en.wikipedia.org/wiki/Étoilé').must_equal 'en.wikipedia.org'
+  end
+
+  it 'should return utf-8 encoded host' do
+    safe_slice_host('http://Étoilé.com/test').must_equal '%C3%89toil%C3%A9.com'
+  end
+end
