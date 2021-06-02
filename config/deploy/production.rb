85fd938379340a15a879efe3b7ace73ffaaa29d0
msk999 <krasnick@synopsys.com>
2021-06-02 07:21:21 -0700

    OTWO-6428 - Changed server config for utility

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 set :web_heads, ['serv-deployer@prd-oh-web04.dc2.lan', 'serv-deployer@prd-oh-web05.dc2.lan',
                  'serv-deployer@prd-oh-web06.dc2.lan']
 
-set :utility, 'serv-deployer@prd-oh-utility01.dc2.lan'
+set :utility, 'serv-deployer@prd-oh-utility02.dc2.lan'
 
 namespace :deploy do
   task started: 'docker:deploy'
