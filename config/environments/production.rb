abe4514919fcd5723d2765fc1a8d6e89df83d365
msk999 <krasnick@synopsys.com>
2021-06-04 11:00:03 -0700

    OTWO-6359 - hot fix to change smtp mail relay address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to
   # raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.smtp_settings = { address: 'mailhost.synopsys.com',
+  config.action_mailer.smtp_settings = { address: 'dmz-smtprelay.synopsys.com',
                                          openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
