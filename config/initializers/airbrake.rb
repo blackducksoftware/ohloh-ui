0d0cc9a67837e01670bfdeaadaecc2eb0ea852a1
msk999 <krasnick@synopsys.com>
2021-07-27 10:25:29 -0700

    OTWO-6437 - ohloh-ui changes for GCP migration

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -3,7 +3,7 @@
 Airbrake.configure do |config|
   config.environment = Rails.env # must be set for ignore_environments to work
   config.project_key = ENV['AIRBRAKE_API_KEY']
-  config.host = "#{ENV['AIRBRAKE_HOST']}:#{ENV['AIRBRAKE_PORT'].to_i}"
+  config.host = ENV['AIRBRAKE_HOST']
   config.project_id = ENV['AIRBRAKE_PROJECT_ID']
   config.ignore_environments = %w[development test]
 end
