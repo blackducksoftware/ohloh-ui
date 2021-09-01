0bea95d9384f6d0300ede6b73fad0247e882cdc9
Priya Rani <prani@blackducksoftware.com>
2021-09-01 15:48:41 +0530

    OTWO-6478 Fixed sidekiq redis configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0", password: ENV['REDIS_PASSWORD'] }
+  redis_config = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0" }
+  redis_config[:password] = ENV['REDIS_PASSWORD'] unless ENV['KUBERNETES_PORT']
+  redis_config[:id] = nil if ENV['KUBERNETES_PORT']
+  config.redis = redis_config
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0", password: ENV['REDIS_PASSWORD'] }
+  redis_config = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0" }
+  redis_config[:password] = ENV['REDIS_PASSWORD'] unless ENV['KUBERNETES_PORT']
+  redis_config[:id] = nil if ENV['KUBERNETES_PORT']
+  config.redis = redis_config
 end
