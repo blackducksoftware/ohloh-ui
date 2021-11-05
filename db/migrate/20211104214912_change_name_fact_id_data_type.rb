2a6c105122645e672f8058715eacdccf7486fb3a
msk999 <krasnick@synopsys.com>
2021-11-04 20:03:05 -0700

    OTWO-6538 Change name_facts.id field type to Bigint

---
+++ db/migrate/20211104214912_change_name_fact_id_data_type.rb
@@ -0,0 +1,27 @@
+class ChangeNameFactIdDataType < ActiveRecord::Migration
+  def up
+    add_column :name_facts, :new_id, :bigint
+    change_field
+
+  end
+
+  def down
+    add_column :name_facts, :new_id, :integer
+    change_field
+  end
+
+  private
+
+  def change_field
+    add_index :name_facts, :new_id, unique: true
+
+    execute <<-SQL
+    alter table name_facts drop constraint name_facts_pkey;
+    alter table name_facts alter new_id set default nextval('name_facts_id_seq'::regclass)
+    update name_facts set new_id = id where new_id is null;
+    alter table name_facts add constraint name_facts_pkey primary key using index my_table_pk_idx;
+    alter table name_facts drop column id;
+    alter table name_facts rename column new_id to id;
+    SQL
+  end
+end
