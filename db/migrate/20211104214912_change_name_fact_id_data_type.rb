dc36c2135316ea37b60909bd7372a61dd5a55b52
msk999 <krasnick@synopsys.com>
2021-11-05 18:44:28 -0700

    OTWO-6538 - Changed SQL - added people table constraint

--- db/migrate/20211104214912_change_name_fact_id_data_type.rb
+++ db/migrate/20211104214912_change_name_fact_id_data_type.rb
@@ -15,13 +15,12 @@ class ChangeNameFactIdDataType < ActiveRecord::Migration
   def change_field
     add_index :name_facts, :new_id, unique: true
 
-    execute <<-SQL
-    alter table name_facts drop constraint name_facts_pkey;
-    alter table name_facts alter new_id set default nextval('name_facts_id_seq'::regclass);
-    update name_facts set new_id = id where new_id is null;
-    alter table name_facts add constraint name_facts_pkey primary key using index my_table_pk_idx;
-    alter table name_facts drop column id;
-    alter table name_facts rename column new_id to id;
-    SQL
+    execute 'alter table name_facts drop constraint name_facts_pkey'
+    execute "alter table name_facts alter new_id set default nextval('name_facts_id_seq'::regclass)";
+    execute 'update name_facts set new_id = id where new_id is null'
+    execute 'alter table name_facts add constraint name_facts_pkey primary key using index my_table_pk_idx'
+    execute 'alter table name_facts drop column id'
+    execute 'alter table name_facts rename column new_id to id'
+    execute 'alter table only oh.people add constraint people_name_fact_id_fkey foreign key (name_fact_id) references oh.name_facts(id) on delete cascade'
   end
 end
