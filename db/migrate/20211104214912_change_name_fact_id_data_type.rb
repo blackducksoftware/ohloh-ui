c1d24ebc28a40bd4788d08522d85a33e521c1769
msk999 <krasnick@synopsys.com>
2021-11-07 06:58:13 -0800

    OTWO-6538 - Removed single SQL statement

--- db/migrate/20211104214912_change_name_fact_id_data_type.rb
+++ db/migrate/20211104214912_change_name_fact_id_data_type.rb
@@ -13,12 +13,13 @@ class ChangeNameFactIdDataType < ActiveRecord::Migration
   private
 
   def change_field
-    add_index :name_facts, :new_id, unique: true
-
-    execute 'alter table name_facts drop constraint name_facts_pkey'
+    execute 'alter table name_facts drop constraint name_facts_pkey cascade'
     execute "alter table name_facts alter new_id set default nextval('name_facts_id_seq'::regclass)";
     execute 'update name_facts set new_id = id where new_id is null'
-    execute 'alter table name_facts add constraint name_facts_pkey primary key using index my_table_pk_idx'
+    execute 'alter table name_facts add primary key (new_id)'
+
+    # We will do the following steps manually in production after verifying the results.
+    # Comment out the next 3 lines in production.
     execute 'alter table name_facts drop column id'
     execute 'alter table name_facts rename column new_id to id'
     execute 'alter table only oh.people add constraint people_name_fact_id_fkey foreign key (name_fact_id) references oh.name_facts(id) on delete cascade'
