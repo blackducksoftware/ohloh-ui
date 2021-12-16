244f9b0e9cbc2bb9ae4f14a92a6310ea78e9f08b
Alex Johnson <notalexjohnson@gmail.com>
2021-12-16 18:34:18 +0530

    Fix out of bounds text issue for datadog error

--- app/lib/data_dog_report.rb
+++ app/lib/data_dog_report.rb
@@ -4,7 +4,8 @@ module DataDogReport
   module_function
 
   def error(message)
-    request = DatadogAPIClient::V1::EventCreateRequest.new(text: message, title: "OpenHub #{Rails.env} Error",
+    request = DatadogAPIClient::V1::EventCreateRequest.new(text: message.truncate(4000),
+                                                           title: "OpenHub #{Rails.env} Error",
                                                            alert_type: :error, date_happened: Time.current.to_i,
                                                            host: ENV['HOSTNAME'])
     api_instance.create_event(request)
