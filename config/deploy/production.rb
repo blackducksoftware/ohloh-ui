b4fa7dd6b3baece54cd5d24f5e0c35f4ecd03b9e
msk999 <krasnick@synopsys.com>
2022-03-22 06:34:49 -0700

    OTWO-6648 - PR Changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ set :web_heads, ['serv-deployer@sdc-oh-web04.prd.sig.synopsys.com',
                  'serv-deployer@sdc-oh-web05.prd.sig.synopsys.com',
                  'serv-deployer@sdc-oh-web06.prd.sig.synopsys.com']
 
-set :utility, 'sdc-oh-utility01.prd.sig.synopsys.com'
+set :utility, 'serv-deployer@sdc-oh-utility01.prd.sig.synopsys.com'
 
 namespace :deploy do
   task started: 'docker:deploy'
