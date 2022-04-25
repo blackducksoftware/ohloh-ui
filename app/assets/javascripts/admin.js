b08e6f585f2a26d3d76b6c16fd22e93d9abe5e0c
Niharika <kumarin@synopsys.com>
2022-04-25 17:09:51 +0530

    OTWO-6635 REFACTOR JS CODE

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,63 +1,23 @@
 var AdminDashboard = {
   init: function() {
-    $("#one_year, #six_months, #three_months_monthly, #six_months_monthly, #one_year_monthly").hide();
+    $('input[name="radio"]').attr("autocomplete", "off");
+    $('.account').hide().filter('#three_months').show();
     $('input[name="options"]').change( function() {
-      AdminDashboard.update_chart($(this).val());
+      AdminDashboard.update_chart($('input[name="radio"]').filter(':checked').val());
     })
     $('input[name="radio"]').change( function() {
       AdminDashboard.update_chart($(this).val());
     })
   },
-  update_chart: function(months) {
-    if(months == '3' && $('input[name="radio"]').filter(':checked').val() == 'weekly') {
-      $("#three_months").show();
-      $("#one_year, #six_months, #three_months_monthly, #six_months_monthly, #one_year_monthly").hide();
+  update_chart: function(filter) {
+    if(filter == 'weekly') {
+      var contClass = $('input[name="options"]').filter(':checked').data('div');
+      $('.account').hide().filter('#' + contClass).show();
     }
-    else if(months == '6' && $('input[name="radio"]').filter(':checked').val() == 'weekly'){
-      $("#six_months").show();
-      $("#one_year, #three_months, #three_months_monthly, #six_months_monthly, #one_year_monthly").hide();
+    else if(filter == 'monthly'){
+      var contClass = $('input[name="options"]').filter(':checked').data('div');
+      $('.account').hide().filter('#' + contClass + '_monthly').show();
     }
-    else if(months == '12' && $('input[name="radio"]').filter(':checked').val() == 'weekly') {
-      $("#one_year").show();
-      $("#six_months, #three_months, #three_months_monthly, #six_months_monthly, #one_year_monthly").hide();
-    }
-    else if(months == 'monthly' && $('input[name="options"]').filter(':checked').val() == '3') {
-      $("#three_months_monthly").show();
-      $("#six_months, #three_months, #one_year, #six_months_monthly, #one_year_monthly").hide();
-    }
-    else if(months == 'weekly' && $('input[name="options"]').filter(':checked').val() == '3') {
-      $("#three_months").show();
-      $("#six_months, #three_months_monthly, #one_year, #six_months_monthly, #one_year_monthly").hide();
-    }
-    else if(months == 'weekly' && $('input[name="options"]').filter(':checked').val() == '6') {
-      $("#six_months").show();
-      $("#three_months, #three_months_monthly, #one_year, #six_months_monthly, #one_year_monthly").hide();
-    }
-    else if(months == '3' && $('input[name="radio"]').filter(':checked').val() == 'monthly') {
-      $("#three_months_monthly").show();
-      $("#three_months, #six_months, #one_year, #six_months_monthly, #one_year_monthly").hide();
-    }
-    else if(months == '6' && $('input[name="radio"]').filter(':checked').val() == 'monthly') {
-      $("#six_months_monthly").show();
-      $("#three_months, #six_months, #one_year, #three_months_monthly, #one_year_monthly").hide();
-    }
-    else if(months == 'monthly' && $('input[name="options"]').filter(':checked').val() == '6') {
-      $("#six_months_monthly").show();
-      $("#three_months, #six_months, #one_year, #three_months_monthly, #one_year_monthly").hide();
-    }
-    else if(months == 'monthly' && $('input[name="options"]').filter(':checked').val() == '12') {
-      $("#one_year_monthly").show();
-      $("#three_months, #six_months, #one_year, #three_months_monthly, #six_months_monthly").hide();
-    }
-    else if(months == '12' && $('input[name="radio"]').filter(':checked').val() == 'monthly') {
-      $("#one_year_monthly").show();
-      $("#three_months, #six_months, #one_year, #three_months_monthly, #six_months_monthly").hide();
-    }
-    else if(months == 'weekly' && $('input[name="options"]').filter(':checked').val() == '12') {
-      $("#one_year").show();
-      $("#three_months, #six_months, #one_year_monthly, #three_months_monthly, #six_months_monthly").hide();
-    }
-
   }
 }
 setTimeout("AdminDashboard.init()", 900);
