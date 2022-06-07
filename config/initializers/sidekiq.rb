67afa705d578d37bb0e07c6dae8bf2173fa6383e
Niharika1117 <101638919+Niharika1117@users.noreply.github.com>
2022-06-07 23:30:43 +0530

    OTWO-6706 removed KUBERENETS_PORT from code (#1620)
    * OTWO-6706 removed KUBERENETS_PORT from code

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,14 +2,14 @@
 
 Sidekiq.configure_server do |config|
   redis_config = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0" }
-  redis_config[:password] = ENV['REDIS_PASSWORD'] unless ENV['KUBERNETES_PORT']
-  redis_config[:id] = nil if ENV['KUBERNETES_PORT']
+  redis_config[:password] = ENV['REDIS_PASSWORD'] if Rails.env.development?
+  redis_config[:id] = nil
   config.redis = redis_config
 end
 
 Sidekiq.configure_client do |config|
   redis_config = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0" }
-  redis_config[:password] = ENV['REDIS_PASSWORD'] unless ENV['KUBERNETES_PORT']
-  redis_config[:id] = nil if ENV['KUBERNETES_PORT']
+  redis_config[:password] = ENV['REDIS_PASSWORD'] if Rails.env.development?
+  redis_config[:id] = nil
   config.redis = redis_config
 end
