c00c35e50d16eaa45ddda27856a6e253c7b66a46
Alex Johnson <notalexjohnson@gmail.com>
2022-06-07 22:55:14 +0530

    OTWO-6718 Handle nil thirty_day_summary for active project

--- test/decorators/home_decorator_test.rb
+++ test/decorators/home_decorator_test.rb
@@ -11,5 +11,22 @@ class HomeDecoratorTest < ActiveSupport::TestCase
       project.best_analysis.thirty_day_summary.update! affiliated_commits_count: commits_count
       _(HomeDecorator.new.commit_count).must_equal [commits_count]
     end
+
+    it 'must handle nil thirty_day_summary values' do
+      project = create(:project)
+
+      commits_count = 5
+      project.best_analysis.thirty_day_summary.update! affiliated_commits_count: commits_count
+
+      bad_project = create(:project)
+      bad_project.best_analysis.thirty_day_summary.destroy
+      bad_project.reload
+
+      home_decorator = HomeDecorator.new
+      # Simulate a scenario where cached most_active_projects has a project which now misses a thirty_day_summary.
+      home_decorator.stubs(:most_active_projects).returns([project, bad_project])
+
+      _(home_decorator.commit_count).must_equal [commits_count]
+    end
   end
 end
