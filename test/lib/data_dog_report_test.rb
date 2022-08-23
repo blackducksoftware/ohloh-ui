e3144fd3535053835b58d215c17187029583bf30
Alex Johnson <notalexjohnson@gmail.com>
2022-08-23 13:47:02 +0530

    Fix DataDog spec

--- test/lib/data_dog_report_test.rb
+++ test/lib/data_dog_report_test.rb
@@ -5,6 +5,6 @@ require 'test_helper'
 class DataDogReportTest < ActiveSupport::TestCase
   it 'must forward the message to rails logger' do
     Rails.logger.expects(:error).once
-    DataDogReport.info('This is a custom event')
+    DataDogReport.error('This is a custom event')
   end
 end
