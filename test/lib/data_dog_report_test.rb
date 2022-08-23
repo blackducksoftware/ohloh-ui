c9c4873b6ac9b6731e5395404bfa952f8900cc6f
Alex <notalexjohnson@gmail.com>
2022-08-23 14:21:56 +0530

    OTWO-6784 Ensure all logs are being directed to DataDog (#1648)
    * OTWO-6784 Ensure all logs are being directed to DataDog

--- test/lib/data_dog_report_test.rb
+++ test/lib/data_dog_report_test.rb
@@ -3,9 +3,8 @@
 require 'test_helper'
 
 class DataDogReportTest < ActiveSupport::TestCase
-  it 'must send a create_event request to Datadog' do
-    VCR.use_cassette('datadog_error_request') do
-      assert_output(/This is error sample/) { DataDogReport.error('This is error sample') }
-    end
+  it 'must forward the message to rails logger' do
+    Rails.logger.expects(:error).once
+    DataDogReport.error('This is a custom event')
   end
 end
