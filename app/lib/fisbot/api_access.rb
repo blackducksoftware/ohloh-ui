070c796ec3806c90f5160fec7a2144cf2acd4140
Alex Johnson <notalexjohnson@gmail.com>
2022-09-01 00:59:30 +0530

    Use class instance variable instead of class var

--- app/lib/fisbot/api_access.rb
+++ app/lib/fisbot/api_access.rb
@@ -3,9 +3,10 @@
 require 'resolv'
 
 class ApiAccess
+  cattr_accessor :fis_ip_url
+
   URL = ENV['FISBOT_API_URL']
   KEY = ENV['FISBOT_CLIENT_REGISTRATION_ID']
-  @@fis_ip_url = nil
 
   def initialize(resource)
     @resource = resource
@@ -14,39 +15,39 @@ class ApiAccess
   def resource_uri(path = nil, query = {})
     path = "/#{path}" if path
     query[:api_key] = KEY
-    URI("#{api_url}/#{@resource}#{path}.json?#{query.to_query}")
+    URI("#{self.class.api_url}/#{@resource}#{path}.json?#{query.to_query}")
   end
 
   class << self
+    def api_url
+      "#{fisbot_resolved_url}/api/v1"
+    end
+
+    private
+
     def fis_public_url
       ENV['FISBOT_PUBLIC_URL'].presence || ENV['FISBOT_API_URL']
     end
-  end
 
-  private
+    def fisbot_resolved_url
+      return URL if Rails.env.development?
+      return fis_ip_url if url_accessible?(fis_ip_url)
 
-  def fisbot_resolved_url
-    return URL if Rails.env.development?
-    return @@fis_ip_url if url_accessible?(@@fis_ip_url)
+      fis_ip_addr = resolve_hostname(URL)
+      self.fis_ip_url = "http://#{fis_ip_addr}"
+    end
 
-    fis_ip_addr = resolve_hostname(URL)
-    @@fis_ip_url = "http://#{fis_ip_addr}"
-  end
+    def url_accessible?(url)
+      return unless url
 
-  def url_accessible?(url)
-    return unless url
+      uri = URI(url)
+      response = Net::HTTP.get_response(uri)
+      response.code == '200'
+    end
 
-    uri = URI(url)
-    response = Net::HTTP.get_response(uri)
-    response.code == '200'
-  end
-
-  def resolve_hostname(url)
-    hostname = url.sub(%r{https?://}, '').sub(/\/$/, '')
-    Resolv.getaddress hostname
-  end
-
-  def api_url
-    "#{fisbot_resolved_url}/api/v1"
+    def resolve_hostname(url)
+      hostname = url.sub(%r{https?://}, '').sub(/\/$/, '')
+      Resolv.getaddress hostname
+    end
   end
 end
--- test/lib/api_access_test.rb
+++ test/lib/api_access_test.rb
@@ -6,18 +6,15 @@ class ApiAccessTest < ActiveSupport::TestCase
   describe 'resolve_hostname' do
     it 'must get the ip address for hostname' do
       stub_constant ApiAccess, :URL, 'http://example.com/' do
-        api_access = ApiAccess.new(:code_locations)
-
-        api_url = api_access.send(:api_url)
+        api_url = ApiAccess.api_url
         _(api_url).must_match %r{^http://\d+\.\d+\.\d+\.\d+/api/v1$}
 
         # Subsequent access to fisbot api
-        api_access = ApiAccess.new(:code_locations)
         Net::HTTP.stubs(:get_response).returns(stub(code: '200'))
-        # We must use cached data.
-        api_access.expects(:resolve_hostname).never
+        # Must use cached data.
+        ApiAccess.expects(:resolve_hostname).never
 
-        api_url = api_access.send(:api_url)
+        api_url = ApiAccess.api_url
         _(api_url).must_match %r{^http://\d+\.\d+\.\d+\.\d+/api/v1$}
       end
     end
