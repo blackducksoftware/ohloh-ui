0fca5663d50c7e5ce1f79cba698abe39527dbdbd
Alex Johnson <notalexjohnson@gmail.com>
2023-01-09 19:22:08 +0530

    Use plural variable name

--- app/helpers/enlistments_helper.rb
+++ app/helpers/enlistments_helper.rb
@@ -27,7 +27,7 @@ module EnlistmentsHelper
     "#{ApiAccess.fis_public_url}/admin/code_locations/#{id}/jobs"
   end
 
-  def code_location_ids_admin_url(id)
-    "#{ApiAccess.fis_public_url}/admin/code_locations?ids=#{id.join(',')}"
+  def code_location_ids_admin_url(ids)
+    "#{ApiAccess.fis_public_url}/admin/code_locations?ids=#{ids.join(',')}"
   end
 end
