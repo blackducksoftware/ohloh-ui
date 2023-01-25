ab41ba3be3c64bb00eedb3b3b0445ac291f7de09
Priya Rani <priya.rani549@gmail.com>
2023-01-25 17:02:02 +0530

    Fixed the query params and the records duplication

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -7,6 +7,7 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = 'Open Hub'
+  config.skip_before_action :strip_query_param
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
