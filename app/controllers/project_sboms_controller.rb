5adaaad26968da25a451b238b1e3e5e899347d03
Niharika1117 <101638919+Niharika1117@users.noreply.github.com>
2023-02-09 16:24:12 +0530

    OTWO-6930 add depecncy package details (#1703)

--- app/controllers/project_sboms_controller.rb
+++ app/controllers/project_sboms_controller.rb
@@ -7,11 +7,23 @@ class ProjectSbomsController < ApplicationController
 
   def index
     @agent = @project.sboms.pluck(:agent).uniq
+    sbom = @project.sboms.where(agent: 'detect_wizard').first&.sbom_data
+    get_package_data(sbom)
   end
 
   def download_json
     project_sbom = @project.sboms.where(agent: params[:agent]).to_json
     filename = "#{@project.name}_#{params[:agent]}.json"
     send_data project_sbom, type: 'application/json;', disposition: "attachment;filename=#{filename}"
+  end
+
+  private
+
+  def get_package_data(sbom)
+    return if sbom.nil?
+
+    @packages = sbom['reportContent'].first['fileContent']['packages']
+                                     .map { |val| val['externalRefs'] }.flatten
+                                     .map { |v| v['referenceLocator'] if v['referenceCategory'] != 'OTHER' }.compact
   end
 end
