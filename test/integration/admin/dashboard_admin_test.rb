f173af8f03ac84f294a42523d007e77df82f94ca
Alex Johnson <notalexjohnson@gmail.com>
2023-02-15 19:54:51 +0530

    Fix specs

--- test/integration/admin/dashboard_admin_test.rb
+++ test/integration/admin/dashboard_admin_test.rb
@@ -13,18 +13,18 @@ class DashboardAdminTest < ActionDispatch::IntegrationTest
 
   it 'renders the dashbord' do
     create(:fetch_job, status: Job::STATUS_RUNNING, current_step_at: Time.current)
-    get admin_root_path
+    get oh_admin_root_path
     assert_response :ok
   end
 
   it 'renders the dashboard when a job is newly running but current_step_at has not been updated' do
     create(:complete_job, status: Job::STATUS_RUNNING, current_step_at: nil)
-    get admin_root_path
+    get oh_admin_root_path
     assert_response :ok
   end
 
   it 'renders the Overviews' do
-    get admin_root_path
+    get oh_admin_root_path
     assert_select 'h1', text: 'Project Stats Overview'
     assert_select 'h1', text: 'Accounts Overview'
     assert_select 'h1', text: 'Last Activities'
