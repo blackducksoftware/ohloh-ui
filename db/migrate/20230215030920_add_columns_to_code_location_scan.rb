a32cf4fc3dde20199446800bf109a47a15a87479
msk999 <krasnick@synopsys.com>
2023-02-22 10:54:42 -0800

    OTWO-6918 - add user_managed column to code_location_scan

--- db/migrate/20230215030920_add_columns_to_code_location_scan.rb
+++ db/migrate/20230215030920_add_columns_to_code_location_scan.rb
@@ -5,5 +5,6 @@ class AddColumnsToCodeLocationScan < ActiveRecord::Migration[5.2]
     add_column :code_location_scan, :language, :string
     add_column :code_location_scan, :command_line, :string
     add_column :code_location_scan, :project_token, :string
+    add_column :code_location_scan, :user_managed, :boolean, default: false
   end
 end
