be3af8054fe9eb4035c21543ef0401b30370bd59
Priya Rani <priya.rani549@gmail.com>
2023-03-09 15:36:09 +0530

    Fixed test failure

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -5,8 +5,7 @@ module MarkdownHelper
     options = { filter_html: true, hard_wrap: true, link_attributes: { rel: 'nofollow', target: '_blank' } }
     renderer = Redcarpet::Render::HTML.new(options)
     extensions = { autolink: true, tables: true, quote: true, highlight: true, strikethrough: true, lax_spacing: true,
-                   fenced_code_blocks: true, disable_indented_code_blocks: true,
-                   space_after_headers: true, no_intra_emphasis: true }
+                   fenced_code_blocks: true, disable_indented_code_blocks: true, space_after_headers: true }
     markdown = Redcarpet::Markdown.new(renderer, extensions)
     markdown.render(text).html_safe
   rescue StandardError
