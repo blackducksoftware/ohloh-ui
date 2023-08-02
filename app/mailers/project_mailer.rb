dd93a4456f85567e01fd2ca47f0a8d28092bdaf8
Priya Rani <priya.rani549@gmail.com>
2023-08-02 20:15:26 +0530

    Update project mailer from address

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class ProjectMailer < ApplicationMailer
+  default from: 'mailer@openhub.net'
+
   def report_outdated(account, project)
+    @account = account
     @project = project
     mail to: 'info@openhub.net', from: account.email, subject: "#{@project.name} is outdated"
   end
