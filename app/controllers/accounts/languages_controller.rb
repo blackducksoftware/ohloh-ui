38110ae5fe9584c31ef88146b0d045bb3684f313
Niharika <kumarin@synopsys.com>
2023-09-14 19:00:11 +0530

    OTWO-7088 nil project_id issue

--- app/controllers/accounts/languages_controller.rb
+++ app/controllers/accounts/languages_controller.rb
@@ -7,7 +7,7 @@ class Accounts::LanguagesController < ApplicationController
   before_action :account_context
 
   def index
-    @contributions = @account.positions.includes(:contribution).map(&:contribution).group_by(&:project_id)
+    @contributions = @account.positions.includes(:contribution).map(&:contribution).compact.group_by(&:project_id)
     return if @account.best_account_analysis.nil?
 
     @vlfs = @account.best_account_analysis.account_analysis_language_facts.with_language_and_projects
