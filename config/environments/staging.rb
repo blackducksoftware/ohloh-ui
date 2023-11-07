5675b736668248bf6edb5942ddfca7c6620f7f73
Niharika <kumarin@synopsys.com>
2023-11-07 17:26:23 +0530

    debug level

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = ENV.fetch('LOG_LEVEL', 'error').to_sym
+  config.log_level = ENV.fetch('LOG_LEVEL', 'debug').to_sym
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
