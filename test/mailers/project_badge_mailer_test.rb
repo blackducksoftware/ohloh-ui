ffce175e8654ba67529c75f0f3ae1fc53013c6e9
Priya Rani <priyar@synopsys.com>
2024-01-30 13:39:09 +0530

    OTWO-7173 Remove hardcoded keys from test env (#1766)
    * OTWO-7173 Remove hardcoded keys from test env
    
    * Update dummy key
    
    * Update env value
    
    * Update cipher key for test
    
    * Update cipher value

--- test/mailers/project_badge_mailer_test.rb
+++ test/mailers/project_badge_mailer_test.rb
@@ -15,7 +15,7 @@ describe ProjectBadgeMailer do
 
     it 'should send email the cii projects detail' do
       email = ProjectBadgeMailer.check_cii_projects(@cii_projects)
-      _(email.to).must_equal ['openhubteam@blackducksoftware.com']
+      _(email.to).must_equal ['ohteam@synopsys.com']
       _(email[:from].value).must_equal 'mailer@openhub.net'
       _(email.subject).must_equal "CII Best Practices Badge - Found #{@cii_projects.size} new projects"
       assert_difference('ActionMailer::Base.deliveries.count', 1) { email.deliver_now }
