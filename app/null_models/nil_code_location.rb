4b363e6072803f9fc9f99e541619a434a23caeea
alex-sig <143193681+alex-sig@users.noreply.github.com>
2024-02-13 16:38:59 +0530

    OTWO-7185 Add more methods to NilCodeLocation (#1767)

--- app/null_models/nil_code_location.rb
+++ app/null_models/nil_code_location.rb
@@ -2,9 +2,13 @@
 
 class NilCodeLocation < NullObject
   blank_methods :url, :nice_url
-  nil_methods :branch
+  nil_methods :branch, :username, :password
 
   def scm_type
     :git
+  end
+
+  def do_not_fetch
+    true
   end
 end
