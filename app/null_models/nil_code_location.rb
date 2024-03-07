e99ab091db75e84280d8e0d4e289926c0533e6d9
alex-sig <143193681+alex-sig@users.noreply.github.com>
2024-03-07 12:56:35 +0530

    OTWO-7202 Enlistment page must render without code location (#1772)

--- app/null_models/nil_code_location.rb
+++ app/null_models/nil_code_location.rb
@@ -2,10 +2,14 @@
 
 class NilCodeLocation < NullObject
   blank_methods :url, :nice_url
-  nil_methods :branch, :username, :password
+  nil_methods :branch, :username, :password, :cl_update_event_time, :best_code_set
 
   def scm_type
     :git
+  end
+
+  def scm_name_in_english
+    :Git
   end
 
   def do_not_fetch
