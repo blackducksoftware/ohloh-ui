5724279d4dc40550e1722b47c48f596b2a561d64
alex-sig <143193681+alex-sig@users.noreply.github.com>
2025-08-19 14:08:17 +0530

    OTWO-7467 Sanitize url when determining branch (#1826)

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -70,6 +70,8 @@ class Api::V1::ProjectsController < ApplicationController
   end
 
   def code_location_branch(url)
+    return unless url&.match(/^(https?|git)/)
+
     out, _err, _status = Open3.capture3("git ls-remote --symref #{url} HEAD | head -1 | awk '{print $2}'")
     out.strip.sub(/refs\/heads\//, '')
   end
