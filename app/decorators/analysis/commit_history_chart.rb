f24f3af018b12e577af37d449f9995a83521a2ab
Niharika1117 <101638919+Niharika1117@users.noreply.github.com>
2026-03-06 14:41:15 +0530

    OTWO-7557 project show page design modification (#1871)
    * OTWO-7557 project show page design modification
    
    * OTWO-7557 fix on the css for project show page

--- app/decorators/analysis/commit_history_chart.rb
+++ app/decorators/analysis/commit_history_chart.rb
@@ -9,9 +9,11 @@ class Analysis::CommitHistoryChart < Analysis::Chart
   end
 
   def data
-    series_and_range_data(@defaults)
+    chart_data = series_and_range_data(@defaults)
       .deep_merge(ANALYSIS_CHARTS_OPTIONS['commits_history_auxillaries'])
       .deep_merge(chart_watermark)
+
+    apply_commits_palette(chart_data)
   end
 
   private
@@ -25,6 +27,15 @@ class Analysis::CommitHistoryChart < Analysis::Chart
   end
 
   def x_and_y_axis_data
+    return [] unless series.last
+
     [{ 'x' => series.last.ticks, 'y' => series.last.commits }]
+  end
+
+  def apply_commits_palette(chart_data)
+    series_config = chart_data[:series] || []
+    series_config[0][:color] = '#2f7ed8' if series_config[0]
+    series_config[1][:color] = '#2f7ed8' if series_config[1]
+    chart_data
   end
 end
