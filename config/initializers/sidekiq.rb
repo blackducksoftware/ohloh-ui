4ae08e832f68bab0130329affe4ade9f06107c31
alex-sig <143193681+alex-sig@users.noreply.github.com>
2026-03-24 12:07:33 +0530

    OTWO-7563 Enable redis password for non test environments (#1865)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,14 +2,14 @@
 
 Sidekiq.configure_server do |config|
   redis_config = { url: "redis://#{ENV.fetch('REDIS_HOST', nil)}:#{ENV.fetch('REDIS_PORT', nil).to_i}/0" }
-  redis_config[:password] = ENV.fetch('REDIS_PASSWORD', nil) if Rails.env.development?
+  redis_config[:password] = ENV.fetch('REDIS_PASSWORD', nil) unless Rails.env.test?
   redis_config[:id] = nil
   config.redis = redis_config
 end
 
 Sidekiq.configure_client do |config|
   redis_config = { url: "redis://#{ENV.fetch('REDIS_HOST', nil)}:#{ENV.fetch('REDIS_PORT', nil).to_i}/0" }
-  redis_config[:password] = ENV.fetch('REDIS_PASSWORD', nil) if Rails.env.development?
+  redis_config[:password] = ENV.fetch('REDIS_PASSWORD', nil) unless Rails.env.test?
   redis_config[:id] = nil
   config.redis = redis_config
 end
