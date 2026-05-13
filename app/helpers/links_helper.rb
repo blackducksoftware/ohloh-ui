f6bc5c9b5100ce767cc1467be95daffdf2f8ae50
kumarin <kumarin@blackduck.com>
2026-05-13 15:42:38 +0530

    Added string extension through the code base

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -4,9 +4,7 @@ require 'addressable/uri'
 
 module LinksHelper
   def safe_slice_host(url, length = 33)
-    # Unescape and force valid UTF-8 before sanitizing
-    unescaped = CGI.unescape(url).encode('UTF-8', invalid: :replace, undef: :replace, replace: '')
-    safe_url = ActionController::Base.helpers.sanitize(unescaped).to_s
+    safe_url = sanitize(CGI.unescape(url).fix_encoding_if_invalid)
     hostname = Addressable::URI.parse(safe_url).host
     truncate(hostname, length: length)
   end
