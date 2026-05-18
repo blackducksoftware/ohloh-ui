27a9cededa3c7c7bf1622d662139dfc24ac720b9
Vaibhav Goyal <vgoyal@blackduck.com>
2026-05-18 12:46:32 +0530

    OTWO-7636 Create and implement Design for Organization show page

--- app/helpers/ratings_helper.rb
+++ app/helpers/ratings_helper.rb
@@ -1,54 +1,41 @@
 # frozen_string_literal: true
 
 module RatingsHelper
-  def rating_stars(id, score, mini: false)
-    dim = rating_star_dimensions(mini)
-    width = ((score.to_f * (dim[:max] - dim[:min]) / 5.0) + 0.5 + dim[:min]).to_i
-    rating_star_html(id, score, dim, width)
+  STAR_PATH = 'M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 ' \
+              '1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034 ' \
+              'a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 ' \
+              '8.72c-.783-.57-.381-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'
+
+  def rating_stars(id, score)
+    "<span id=\"#{id}\"> \
+      #{rating_star_schema(score)} \
+      #{svg_star_rating(score)} \
+    </span>"
   end
 
   private
 
-  def rating_star_html(id, score, dim, width)
-    return rating_star_worst(id, score, dim) if width <= dim[:min]
-    return rating_star_best(id, score, dim) if width >= dim[:max]
+  def svg_star_rating(score)
+    stars_html = (1..5).map do |star|
+      star_filled = star <= score.to_f
+      half_filled = !star_filled && (star - 0.5) <= score.to_f
 
-    rstarfull(id, score, "#{rating_star_row(dim, width, false)} #{rating_star_row(dim, dim[:max] - width, true)}")
+      color = star_filled || half_filled ? '#ffb91a' : '#d1d5db'
+      build_star_svg(color)
+    end.join
+
+    "<span style=\"display: flex; align-items: center; gap: 2px;\">#{stars_html}</span>"
   end
 
-  def rating_star_worst(id, score, dim)
-    rstarfull(id, score, rating_star_row(dim, dim[:max], true))
-  end
-
-  def rating_star_best(id, score, dim)
-    rstarfull(id, score, rating_star_row(dim, dim[:max], false))
-  end
-
-  def rating_star_dimensions(mini)
-    return { max: 76, min: 1, height: 17, img: 'stars_sprite_mini.png' } if mini
-
-    { max: 112, min: 2, height: 23, img: 'stars_sprite.png' }
-  end
-
-  def rstarfull(id, score, ratings)
-    "<span id=\"#{id}\"> \
-      #{rating_star_schema(score)} #{ratings} \
-    </span>"
+  def build_star_svg(color)
+    '<svg style="width: 14px; height: 14px; display: inline; margin-right: 2px; ' \
+      "color: #{color}; fill: currentColor;\" viewBox=\"0 0 20 20\"> " \
+      "<path d=\"#{STAR_PATH}\"></path> </svg>"
   end
 
   def rating_star_schema(score)
-    <<-HTML
-    <div style = "display:none;" itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
-      <span style = "display:none;" itemprop="ratingValue">#{score}</span>
-    </div>
-    HTML
-  end
-
-  def rating_star_row(dimensions, width, empty)
-    position = empty ? 'right top' : 'left bottom'
-    "<span style=\"font-size: 1px; float: left; display: inline; height: #{dimensions[:height]}px; width: #{width}px; \
-                   background: url(" + image_path("rating_stars/#{dimensions[:img]}") + ") #{position}\"> \
-      &nbsp; \
-    </span>"
+    '<div style="display:none;" itemprop="aggregateRating" itemscope ' \
+      'itemtype="http://schema.org/AggregateRating"> ' \
+      "<span style=\"display:none;\" itemprop=\"ratingValue\">#{score}</span> </div>"
   end
 end
