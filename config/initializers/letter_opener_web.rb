92024f7cf64de6c2daaa0cbce02bb256944612ee
ashutok036221 <ashutok@blackduck.com>
2026-07-28 15:58:43 +0530

    OTWO:7701: Added check to redirect to sessions/new page when unauthor… (#1927)
    * OTWO:7701: Added check to redirect to sessions/new page when unauthorized user access /inbox
    
    * OTWO-7701: robocop issue fix

---
+++ config/initializers/letter_opener_web.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+Rails.application.config.to_prepare do
+  next unless defined?(LetterOpenerWeb::LettersController)
+
+  LetterOpenerWeb::LettersController.class_eval do
+    before_action :check_access
+
+    private
+
+    def check_access
+      redirect_to main_app.new_session_path if request.env[:clearance].current_user.nil?
+    end
+  end
+end
