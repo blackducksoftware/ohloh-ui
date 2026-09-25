28f0f1b8fcea348240e16ece6ee53b128b99122c
Niharika1117 <101638919+Niharika1117@users.noreply.github.com>
2026-09-25 11:45:11 +0530

    OTWO-7736 Update OpenHub S3 Upload Configuration for Private Bucket (#1952)

--- config/environment.rb
+++ config/environment.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
       secret_access_key: ENV.fetch('OHLOH_S3_SECRET_ACCESS_KEY', nil)
     },
     s3_region: ENV.fetch('AWS_REGION', nil),
+    s3_permissions: :private,
     s3_protocol: :https,
     s3_host_name: 's3.amazonaws.com',
     endpoint: 'https://s3.amazonaws.com'
